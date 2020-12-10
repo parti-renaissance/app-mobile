54b12cb84b8468440ccf73f0a5cd7d6ee4621214
Francois Lolom <francois.lolom@fabernovel.com>
2020-12-10 15:40:49 +0100

    Apply scroll only for android

--- src/screens/pollDetail/PollDetailQuestionInput.tsx
+++ src/screens/pollDetail/PollDetailQuestionInput.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent, useRef, useState } from 'react'
-import { Text, StyleSheet, View, TextInput } from 'react-native'
+import { Text, StyleSheet, View, TextInput, Platform } from 'react-native'
 import { Colors, Spacing, Typography } from '../../styles'
 import i18n from '../../utils/i18n'
 import { PollDetailQuestionInputViewModel } from './PollDetailQuestionInputViewModel'
@@ -12,7 +12,7 @@ type Props = Readonly<{
   onChangeText: (text: string) => void
 }>
 
-const PollDetailQuestionInput: FunctionComponent<Props> = ({
+const PollDetailQuestionInputContent: FunctionComponent<Props> = ({
   viewModel,
   onChangeText,
 }) => {
@@ -27,36 +27,49 @@ const PollDetailQuestionInput: FunctionComponent<Props> = ({
     setRemainingCharsLabel(computeRemainingCharsLabel(text))
     onChangeText(text)
   }
-
   return (
-    <KeyboardOffsetView>
-      <ScrollView>
-        <View style={styles.container}>
-          <Text style={styles.title}>{viewModel.title}</Text>
-          <View style={styles.inner}>
-            <TextInput
-              ref={inputRef}
-              style={styles.input}
-              multiline={true}
-              value={viewModel.content}
-              placeholder={i18n.t('polldetail.question_placeholder')}
-              inputAccessoryViewID={inputAccessoryViewId}
-              onChangeText={textChanged}
-              maxLength={MAX_CHARACTERS}
-            />
-            <Text style={styles.remaingCharsIndicator}>
-              {remaingCharsLabel}
-            </Text>
-            <InputAccessoryClose
-              id={inputAccessoryViewId}
-              title={i18n.t('common.keyboard.done')}
-              onPress={() => inputRef.current?.blur()}
-            />
-          </View>
-        </View>
-      </ScrollView>
-    </KeyboardOffsetView>
+    <>
+      <Text style={styles.title}>{viewModel.title}</Text>
+      <View style={styles.inner}>
+        <TextInput
+          ref={inputRef}
+          style={styles.input}
+          multiline={true}
+          value={viewModel.content}
+          placeholder={i18n.t('polldetail.question_placeholder')}
+          inputAccessoryViewID={inputAccessoryViewId}
+          onChangeText={textChanged}
+          maxLength={MAX_CHARACTERS}
+        />
+        <Text style={styles.remaingCharsIndicator}>{remaingCharsLabel}</Text>
+        <InputAccessoryClose
+          id={inputAccessoryViewId}
+          title={i18n.t('common.keyboard.done')}
+          onPress={() => inputRef.current?.blur()}
+        />
+      </View>
+    </>
   )
+}
+
+const PollDetailQuestionInput: FunctionComponent<Props> = (props) => {
+  if (Platform.OS === 'android') {
+    return (
+      <KeyboardOffsetView>
+        <ScrollView>
+          <View style={styles.container}>
+            <PollDetailQuestionInputContent {...props} />
+          </View>
+        </ScrollView>
+      </KeyboardOffsetView>
+    )
+  } else {
+    return (
+      <KeyboardOffsetView style={styles.container}>
+        <PollDetailQuestionInputContent {...props} />
+      </KeyboardOffsetView>
+    )
+  }
 }
 
 const MAX_CHARACTERS = 255
@@ -73,6 +86,7 @@ const styles = StyleSheet.create({
   },
   inner: {
     flex: 1,
+    marginBottom: Spacing.unit,
   },
   title: {
     ...Typography.headline,
