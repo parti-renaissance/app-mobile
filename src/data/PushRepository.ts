09a3fca42faefd701603dda1a163458d0cf3b870
Francois Lolom <francois.lolom@fabernovel.com>
2020-12-11 12:23:40 +0100

    Register to topic when opening app

--- src/data/PushRepository.ts
+++ src/data/PushRepository.ts
@@ -1,6 +1,18 @@
+import { PushTopic } from '../core/entities/PushTopic'
+import messaging from '@react-native-firebase/messaging'
+import { ENVIRONMENT } from '../Config'
+
 class PushRepository {
   private static instance: PushRepository
   private constructor() {}
+
+  public async subscribeToTopic(topic: PushTopic): Promise<void> {
+    return messaging().subscribeToTopic(this.createTopicName(topic))
+  }
+
+  private createTopicName(topic: PushTopic): string {
+    return ENVIRONMENT + '_' + topic
+  }
 
   public static getInstance(): PushRepository {
     if (!PushRepository.instance) {
