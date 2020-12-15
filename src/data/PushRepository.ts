cf0caaf763873d575f8a6f4a2bae90b4344d2722
Francois Lolom <francois.lolom@fabernovel.com>
2020-12-15 16:21:10 +0100

    Update topic only if registration changed

--- src/data/PushRepository.ts
+++ src/data/PushRepository.ts
@@ -1,20 +1,31 @@
-import { PushTopic } from '../core/entities/PushTopic'
 import messaging from '@react-native-firebase/messaging'
 import { ENVIRONMENT } from '../Config'
+import LocalStore from './store/LocalStore'
 
 class PushRepository {
   private static instance: PushRepository
+  private localStore = LocalStore.getInstance()
   private constructor() {}
 
-  public async subscribeToTopic(topic: PushTopic): Promise<void> {
-    return messaging().subscribeToTopic(this.createTopicName(topic))
+  public async subscribeToGeneralTopic(): Promise<void> {
+    const registrations = await this.localStore.getTopicsRegistration()
+    if (registrations?.globalRegistered !== true) {
+      await messaging().subscribeToTopic(this.createTopicName('global'))
+      await this.localStore.updateTopicsRegistration({
+        globalRegistered: true,
+      })
+    } else {
+      // no-op: already registered
+    }
   }
 
   public async invalidatePushToken(): Promise<void> {
-    return messaging().deleteToken()
+    return messaging()
+      .deleteToken()
+      .then(() => this.localStore.clearTopicsRegistration())
   }
 
-  private createTopicName(topic: PushTopic): string {
+  private createTopicName(topic: string): string {
     return ENVIRONMENT + '_jemarche_' + topic
   }
 
--- src/data/store/LocalStore.ts
+++ src/data/store/LocalStore.ts
@@ -1,9 +1,11 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { UserPreferences } from '../../core/entities/UserPreferences'
 import { Credentials } from './Credentials'
+import { TopicsRegistration } from './TopicsRegistration'
 
 const KEY_CREDENTIALS = 'credentials'
 const USER_PREFERENCES = 'userPreferences'
+const TOPICS_REGISTRATION = 'topicsRegistration'
 
 class LocalStore {
   private static instance: LocalStore
@@ -38,6 +40,18 @@ class LocalStore {
     return AsyncStorage.removeItem(USER_PREFERENCES)
   }
 
+  updateTopicsRegistration(registration: TopicsRegistration): Promise<void> {
+    return this.storeTopicsRegistration(registration)
+  }
+
+  getTopicsRegistration(): Promise<TopicsRegistration | null> {
+    return AsyncStorage.getItem(TOPICS_REGISTRATION).then(this.parseJSON)
+  }
+
+  clearTopicsRegistration(): Promise<void> {
+    return AsyncStorage.removeItem(TOPICS_REGISTRATION)
+  }
+
   public static getInstance(): LocalStore {
     if (!LocalStore.instance) {
       LocalStore.instance = new LocalStore()
@@ -56,6 +70,19 @@ class LocalStore {
     return AsyncStorage.setItem(
       USER_PREFERENCES,
       JSON.stringify(newPreferences),
+    )
+  }
+
+  private async storeTopicsRegistration(
+    object: {
+      [key in keyof TopicsRegistration]?: any
+    },
+  ): Promise<void> {
+    const topicsRegistration = await this.getTopicsRegistration()
+    const newTopicsRegistration = { ...topicsRegistration, ...object }
+    return AsyncStorage.setItem(
+      TOPICS_REGISTRATION,
+      JSON.stringify(newTopicsRegistration),
     )
   }
 
