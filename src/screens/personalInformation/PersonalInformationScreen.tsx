21badedf6271e43d39e3d1ddd03857177d791333
Francois Lolom <francois.lolom@fabernovel.com>
2021-03-09 17:57:42 +0100

    Add certified block

--- src/screens/personalInformation/PersonalInformationScreen.tsx
+++ src/screens/personalInformation/PersonalInformationScreen.tsx
@@ -1,10 +1,30 @@
 import React, { FC } from 'react'
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { Colors } from '../../styles'
+import CertifiedProfileView from './CertifiedProfileView'
 
 type Props = Readonly<{}>
 
 const PersonalInformationScreen: FC<Props> = () => {
-  return <View />
+  const isCertified = false
+  return (
+    <View style={styles.container}>
+      <CertifiedProfileView
+        style={styles.certifiedContainer}
+        isCertified={isCertified}
+      />
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.defaultBackground,
+  },
+  certifiedContainer: {
+    margin: 16,
+  },
+})
 
 export default PersonalInformationScreen
