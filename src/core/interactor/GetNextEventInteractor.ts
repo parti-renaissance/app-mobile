2874d23ec3268a7dcde5b2142442ff3db0be0320
Pierre Felgines <pierre.felgines@fabernovel.com>
2021-04-20 09:44:16 +0200

    Add rules for next event

--- src/core/interactor/GetNextEventInteractor.ts
+++ src/core/interactor/GetNextEventInteractor.ts
@@ -2,6 +2,7 @@ import EventRepository from '../../data/EventRepository'
 import ProfileRepository from '../../data/ProfileRepository'
 import { EventFilters, ShortEvent } from '../entities/Event'
 import { DateProvider } from '../../utils/DateProvider'
+import moment from 'moment'
 
 export class GetNextEventInteractor {
   private eventRepository = EventRepository.getInstance()
@@ -15,6 +16,15 @@ export class GetNextEventInteractor {
       subscribedOnly: true,
     }
     const events = await this.eventRepository.getEvents(zipCode, page, filters)
-    return events.result[0]
+    let now = moment(DateProvider.now())
+    const sortedEvents = events.result
+      .filter((event) => {
+        const eventIsToday = event.dateStart.isSame(now, 'day')
+        const eventEndIsLater = event.dateEnd.isAfter(now)
+        return eventIsToday && eventEndIsLater
+      })
+      .sort((lhs, rhs) => (lhs.dateStart.isBefore(rhs.dateStart) ? -1 : 1))
+    const shouldDisplayEvents = now.hour() >= 7 // dislay events only after 7 a.m
+    return shouldDisplayEvents ? sortedEvents[0] : undefined
   }
 }
