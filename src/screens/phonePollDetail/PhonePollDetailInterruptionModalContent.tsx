f71c4abce56dfe6742cda67482f3298b03e38811
Pierre Felgines <pierre.felgines@fabernovel.com>
2021-09-10 10:31:07 +0200

    Remove useless group

--- src/screens/phonePollDetail/PhonePollDetailInterruptionModalContent.tsx
+++ src/screens/phonePollDetail/PhonePollDetailInterruptionModalContent.tsx
@@ -43,27 +43,23 @@ const PhonePollDetailInterruptionModalContent: FunctionComponent<Props> = ({
   }
 
   return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          {i18n.t('phonepolldetail.interruption_alert.title')}
-        </Text>
-        <FlatList
-          bounces={false}
-          data={viewModel.choices}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={renderItem}
-        />
-        <PrimaryButton
-          style={styles.button}
-          disabled={!viewModel.isActionEnabled}
-          onPress={() =>
-            selectedStatusCode && onInterruption(selectedStatusCode)
-          }
-          title={i18n.t('phonepolldetail.interruption_alert.action')}
-        />
-      </View>
-    </>
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        {i18n.t('phonepolldetail.interruption_alert.title')}
+      </Text>
+      <FlatList
+        bounces={false}
+        data={viewModel.choices}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderItem}
+      />
+      <PrimaryButton
+        style={styles.button}
+        disabled={!viewModel.isActionEnabled}
+        onPress={() => selectedStatusCode && onInterruption(selectedStatusCode)}
+        title={i18n.t('phonepolldetail.interruption_alert.action')}
+      />
+    </View>
   )
 }
 
