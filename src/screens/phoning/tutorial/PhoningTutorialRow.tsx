8a03a352af7034308628eaeed700a4ebef53e064
Denis Poifol <denis.poifol@gmail.com>
2021-09-10 09:44:13 +0200

    Use borderlessButton instead of text

--- src/screens/phoning/tutorial/PhoningTutorialRow.tsx
+++ src/screens/phoning/tutorial/PhoningTutorialRow.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Spacing, Typography } from '../../../styles'
-import { useThemedStyles } from '../../../themes'
+import { Spacing, Typography, Styles } from '../../../styles'
+import { useThemedStyles, useTheme } from '../../../themes'
 import Theme from '../../../themes/Theme'
+import { BorderlessButton } from '../../shared/Buttons'
 
 type Props = Readonly<{
   onPress: () => void
@@ -14,13 +15,17 @@ export interface PhoningTutorialRowViewModel {
 
 export const PhoningTutorialRow: FunctionComponent<Props> = ({ onPress }) => {
   const styles = useThemedStyles(stylesFactory)
+  const { theme } = useTheme()
   return (
     <View style={styles.card}>
       <View style={styles.container}>
         <Text style={styles.title}>{'Comment faire un appel'}</Text>
-        <Text style={styles.link} accessibilityRole={'link'} onPress={onPress}>
-          {'regarder le tutoriel'}
-        </Text>
+        <BorderlessButton
+          title={'regarder le tutoriel'}
+          textStyle={styles.linkText}
+          style={styles.linkButton}
+          onPress={onPress}
+        />
       </View>
     </View>
   )
@@ -37,22 +42,25 @@ const stylesFactory = (theme: Theme) => {
     container: {
       flex: 1,
       flexDirection: 'column',
-      alignItems: 'center',
       paddingRight: 8 * Spacing.unit,
     },
-    link: {
-      ...Typography.headline,
-      flexShrink: 1,
-      padding: 16,
-      paddingTop: 5,
-      alignSelf: 'flex-start',
+    linkText: {
+      ...Styles.eventSeeMoreButtonTextStyle(theme),
       color: theme.primaryColor,
+    },
+    linkButton: {
+      flexShrink: 1,
+      padding: Spacing.margin,
+      paddingLeft: -Spacing.margin,
+      paddingTop: Spacing.small,
+      textAlign: 'left',
+      alignSelf: 'flex-start',
     },
     title: {
       ...Typography.headline,
       flexShrink: 1,
-      padding: 16,
-      paddingBottom: 5,
+      padding: Spacing.margin,
+      paddingBottom: Spacing.small,
       alignSelf: 'flex-start',
     },
   })
