8802091d6d74c2136ce401e1a7facc2877c4dd62
Romain Goutte-Fangeas <romain.goutte@fabernovel.com>
2021-09-13 15:21:57 +0200

    Style bottom navigation buttons

--- src/screens/phoningCampaignBrief/PhoningCampaignBriefScreen.tsx
+++ src/screens/phoningCampaignBrief/PhoningCampaignBriefScreen.tsx
@@ -1,48 +1,70 @@
 import React, { FunctionComponent } from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, ScrollView, View } from 'react-native'
 import SafeAreaView from 'react-native-safe-area-view'
 import { PhoningCampaignBriefScreenProp, Screen } from '../../navigation'
-import { Colors, Spacing } from '../../styles'
-import { PrimaryButton, SecondaryButton } from '../shared/Buttons'
+import { Colors, Spacing, Styles, Typography } from '../../styles'
+import { title } from '../../styles/typography'
+import { useThemedStyles } from '../../themes'
+import Theme from '../../themes/Theme'
+import i18n from '../../utils/i18n'
+import { BorderlessButton, PrimaryButton } from '../shared/Buttons'
 import { VerticalSpacer } from '../shared/Spacer'
 
 const PhoningCampaignBriefScreen: FunctionComponent<PhoningCampaignBriefScreenProp> = ({
   navigation,
   route,
 }) => {
+  const styles = useThemedStyles(styleFactory)
   return (
     <SafeAreaView style={styles.container}>
-      <Text>PhoningSessionLoaderScreen</Text>
-      <VerticalSpacer spacing={Spacing.margin} />
-      <PrimaryButton
-        title="_LANCER_APPEL_"
-        onPress={() =>
-          navigation.navigate(Screen.phoningSessionModal, {
-            screen: Screen.phoningSessionLoader,
-            params: { campaignId: route.params.campaignId, device: 'current' },
-          })
-        }
-      />
-      <VerticalSpacer spacing={Spacing.margin} />
-      <SecondaryButton
-        title="_LANCER_APPEL_AUTRE_TEL_"
-        onPress={() =>
-          navigation.navigate(Screen.phoningSessionModal, {
-            screen: Screen.phoningSessionLoader,
-            params: { campaignId: route.params.campaignId, device: 'external' },
-          })
-        }
-      />
+      <ScrollView style={styles.contentContainer}>
+        <VerticalSpacer spacing={Spacing.margin} />
+      </ScrollView>
+      <View style={styles.bottomContainer}>
+        <PrimaryButton
+            title={i18n.t('phoning.brief.call')}
+            onPress={() =>
+              navigation.navigate(Screen.phoningSessionModal, {
+                screen: Screen.phoningSessionLoader,
+                params: { campaignId: route.params.campaign.id, device: 'current' },
+              })
+            }
+          />
+          <BorderlessButton
+            title={i18n.t('phoning.brief.call_from_other_device')}
+            textStyle={styles.linkText}
+            onPress={() =>
+              navigation.navigate(Screen.phoningSessionModal, {
+                screen: Screen.phoningSessionLoader,
+                params: { campaignId: route.params.campaign.id, device: 'external' },
+              })
+            }
+          />
+      </View>
     </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Colors.defaultBackground,
-    flex: 1,
-    paddingHorizontal: Spacing.margin,
-  },
-})
+const styleFactory = (theme: Theme) => {
+  return StyleSheet.create({
+    container: {
+      backgroundColor: Colors.defaultBackground,
+      flex: 1,
+    },
+    contentContainer: {
+      paddingHorizontal: Spacing.margin,
+    },
+    linkText: {
+      ...Styles.eventSeeMoreButtonTextStyle(theme),
+      color: theme.primaryColor,
+    },
+    bottomContainer: {
+      ...Styles.topElevatedContainerStyle,
+      backgroundColor: Colors.defaultBackground,
+      paddingHorizontal: Spacing.margin,
+      paddingTop: Spacing.margin,
+    },
+  })
+}
 
 export default PhoningCampaignBriefScreen
