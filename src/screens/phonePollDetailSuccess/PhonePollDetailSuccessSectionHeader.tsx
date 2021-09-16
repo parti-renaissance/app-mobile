49ec18ca0ad0c65146f255a159ab2acae2cab318
Pierre Felgines <pierre.felgines@fabernovel.com>
2021-09-16 16:36:57 +0200

    Display ranking on success screen

---
+++ src/screens/phonePollDetailSuccess/PhonePollDetailSuccessSectionHeader.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import { Spacing, Typography } from '../../styles'
+import { VerticalSpacer } from '../shared/Spacer'
+
+type Props = Readonly<{
+  title: string
+}>
+
+export const PhonePollDetailSuccessSectionHeader: FunctionComponent<Props> = ({
+  title,
+}) => {
+  return (
+    <View style={styles.content}>
+      <VerticalSpacer spacing={Spacing.largeMargin} />
+      <Text style={styles.title}>{title}</Text>
+      <VerticalSpacer spacing={Spacing.margin} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  content: {
+    paddingHorizontal: Spacing.margin,
+  },
+  title: {
+    ...Typography.title,
+  },
+})
