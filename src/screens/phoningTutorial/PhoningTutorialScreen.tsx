8ac796d0c4316ed0536415833f8ef96734cd74ef
Denis Poifol <denis.poifol@gmail.com>
2021-09-16 12:17:55 +0200

    Add loading and error handling

--- src/screens/phoningTutorial/PhoningTutorialScreen.tsx
+++ src/screens/phoningTutorial/PhoningTutorialScreen.tsx
@@ -1,4 +1,9 @@
-import React, { FunctionComponent, useEffect, useState } from 'react'
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
 import { StyleSheet, ScrollView } from 'react-native'
 import Markdown from 'react-native-markdown-display'
 import SafeAreaView from 'react-native-safe-area-view'
@@ -6,6 +11,9 @@ import PhoningCampaignRepository from '../../data/PhoningCampaignRepository'
 import { Colors, Spacing } from '../../styles'
 import { PhoningTutorialScreenProp } from '../../navigation'
 import i18n from '../../utils/i18n'
+import { GenericErrorMapper } from '../shared/ErrorMapper'
+import { StatefulView, ViewState } from '../shared/StatefulView'
+import { useFocusEffect } from '@react-navigation/core'
 
 export interface TutorialResources {
   content: string
@@ -14,26 +22,46 @@ export interface TutorialResources {
 const PhoningTutorialScreen: FunctionComponent<PhoningTutorialScreenProp> = ({
   navigation,
 }) => {
+  const [statefulState, setStatefulState] = useState<
+    ViewState.Type<TutorialResources>
+  >(new ViewState.Loading())
   const [currentResources, setResources] = useState<TutorialResources>()
 
   useEffect(() => {
     navigation.setOptions({
       title: i18n.t('phoning.tutorial.title'),
     })
+    setStatefulState(new ViewState.Content(currentResources))
+  }, [navigation, currentResources])
+
+  const fetchData = useCallback(() => {
+    setStatefulState(new ViewState.Loading())
     PhoningCampaignRepository.getInstance()
       .getPhoningTutorial()
       .then((markdown) => {
-        setResources({
-          content: markdown,
-        })
+        setResources({ content: markdown })
+      })
+      .catch((error) => {
+        setStatefulState(
+          new ViewState.Error(GenericErrorMapper.mapErrorMessage(error), () => {
+            fetchData()
+          }),
+        )
       })
   }, [])
 
-  return (
-    <SafeAreaView style={styles.container}>
+  useFocusEffect(fetchData)
+
+  const TutorialContent = () => {
+    return (
       <ScrollView style={styles.contentContainer}>
         <Markdown>{currentResources?.content}</Markdown>
       </ScrollView>
+    )
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatefulView contentComponent={TutorialContent} state={statefulState} />
     </SafeAreaView>
   )
 }
