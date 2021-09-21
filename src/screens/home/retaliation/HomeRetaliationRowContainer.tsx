7672e3998656fece01db3db0738d4995abe9c5f7
Denis Poifol <denis.poifol@gmail.com>
2021-09-21 10:51:44 +0200

    Add explicit typing

--- src/screens/home/retaliation/HomeRetaliationRowContainer.tsx
+++ src/screens/home/retaliation/HomeRetaliationRowContainer.tsx
@@ -2,8 +2,9 @@ import React, { FunctionComponent, useState } from 'react'
 import { HomeRetaliationRowContainerViewModel } from '../HomeRowViewModel'
 import RetaliationCard from './RetaliationCard'
 import Carousel, { Pagination } from 'react-native-snap-carousel'
-import { Dimensions } from 'react-native'
+import { Dimensions, ListRenderItemInfo } from 'react-native'
 import { Spacing, Colors } from '../../../styles'
+import { RetaliationCardViewModel } from './RetaliationCardViewModel'
 
 type Props = Readonly<{
   viewModel: HomeRetaliationRowContainerViewModel
@@ -23,7 +24,7 @@ export const HomeRetaliationRowContainer: FunctionComponent<Props> = ({
         sliderWidth={width}
         itemWidth={width - Spacing.margin * 2}
         itemHeight={280}
-        renderItem={(model) => {
+        renderItem={(model: ListRenderItemInfo<RetaliationCardViewModel>) => {
           return <RetaliationCard viewModel={model.item} />
         }}
         onSnapToItem={(index) => setCurrentItem(index)}
