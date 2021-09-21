7a7359ab551ae8d2cd14150c902933b7770e7fac
Romain Goutte-Fangeas <romain.goutte@fabernovel.com>
2021-09-21 11:23:18 +0200

    Create fake data

--- src/screens/phonePollSatisfaction/PhonePollSatisfactionViewModelMapper.ts
+++ src/screens/phonePollSatisfaction/PhonePollSatisfactionViewModelMapper.ts
@@ -46,6 +46,82 @@ export const PhonePollSatisfactionViewModelMapper = {
         }
       },
     )
+
+    // TODO (Romain GF) 20/09/2021 Use real data when back ready
+    const inputId = 'test-input-id'
+    const inputAnswer = answers.get(inputId)
+    sections.splice(0, 0, {
+      id: inputId,
+      title: '_INPUT_TITLE_',
+      data: [
+        {
+          type: 'input',
+          value: {
+            id: inputId,
+            text:
+              inputAnswer && inputAnswer?.type === 'input'
+                ? inputAnswer.value
+                : '',
+          },
+        },
+      ],
+    })
+
+    // TODO (Romain GF) 20/09/2021 Use real data when back ready
+    const rateId = 'test-rate-id'
+    const answer = answers.get(rateId)
+    const rate =
+      answer && answer?.type === 'rate' ? (answer?.value as number) ?? 0 : 0
+    sections.splice(1, 0, {
+      id: rateId,
+      title: '_RATE_TITLE_',
+      data: [
+        {
+          type: 'rate',
+          value: {
+            id: rateId,
+            rateMaximum: 5,
+            rate: rate,
+            subtitle: '_RATE_QUESTION_SUBTITLE_',
+          },
+        },
+      ],
+    })
+
+    // TODO (Romain GF) 20/09/2021 Use real data when back ready
+    const choiceId = 'test-choice-id'
+    const selectedChoiceId = answers.get(choiceId)?.value
+    sections.push({
+      id: choiceId,
+      title: '_CHOICE_TITLE_',
+      data: [
+        {
+          type: 'single_choice',
+          value: {
+            id: choiceId,
+            subtitle: '_CHOICE_SUBTITLE_',
+            answers: [
+              {
+                id: '1',
+                title: '_CHOICE_ONE_',
+                isSelected: selectedChoiceId === '1',
+              },
+              {
+                id: '2',
+                title: '_CHOICE_TWO_',
+                isSelected: selectedChoiceId === '2',
+              },
+              {
+                id: '3',
+                title: '_CHOICE_THREE_',
+                isSelected: selectedChoiceId === '3',
+              },
+            ],
+          },
+        },
+      ],
+    })
+
     return { sections }
   },
 }
