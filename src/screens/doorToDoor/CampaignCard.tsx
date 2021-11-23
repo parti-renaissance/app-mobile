570e59434fb5b2e2f7dff226c7daf6f71ac70d3e
ChawkiOS <chawkios2@gmail.com>
2021-11-23 23:24:11 +0100

    create campaign card

---
+++ src/screens/doorToDoor/CampaignCard.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Colors, Spacing, Typography } from '../../styles'
+import { useTheme } from '../../themes'
+import CardView from '../shared/CardView'
+import ProgressBar from '../shared/ProgressBar'
+
+export const CampaignCard = () => {
+  const { theme } = useTheme()
+
+  return (
+    <CardView backgroundColor={Colors.defaultBackground}>
+      <View style={styles.content}>
+        <View style={styles.campaign}>
+          <Text style={styles.title}>Campagne nationale</Text>
+          <Text style={styles.date}>jusqu’au 3 jan. 2022</Text>
+        </View>
+        <View>
+          <Text style={styles.goal}>
+            Mon objectif : <Text style={styles.indicator}>03/30</Text>
+          </Text>
+          <ProgressBar progress={0.3} color={theme.primaryColor} />
+        </View>
+      </View>
+    </CardView>
+  )
+}
+const styles = StyleSheet.create({
+  campaign: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  content: {
+    padding: Spacing.margin,
+    flexDirection: 'row',
+  },
+  date: {
+    ...Typography.lightCallout,
+    marginTop: Spacing.small,
+  },
+  goal: {
+    ...Typography.lightCallout,
+    marginBottom: Spacing.unit,
+  },
+  indicator: {
+    ...Typography.headline,
+  },
+  title: {
+    ...Typography.title2,
+  },
+})
