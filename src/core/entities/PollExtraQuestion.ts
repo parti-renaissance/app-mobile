41c75472aa428d8cb6d3616c1d5bd36f72a58508
Romain Goutte-Fangeas <romain.goutte@fabernovel.com>
2021-12-16 16:52:05 +0100

    Map survey config qualification questions

---
+++ src/core/entities/PollExtraQuestion.ts
@@ -0,0 +1,49 @@
+export interface PollExtraQuestionPage {
+  description: string | null
+  questions: Array<PollExtraQuestion>
+}
+
+export type PollExtraQuestionType = 'choice' | 'text' | 'compound'
+
+export interface PollExtraQuestion {
+  code: string
+  type: PollExtraQuestionType
+  dependency?: PollExtraQuestionDependency
+  options: PollExtraQuestionOptions
+}
+
+export interface PollExtraQuestionDependency {
+  question: string
+  choices: Array<string | boolean>
+}
+
+export interface PollExtraQuestionChoiceOptions {
+  title: string
+  subtitle?: string
+  required: boolean
+  columns: number
+  multiple: boolean
+  choices: Array<PollExtraQuestionChoice>
+}
+
+export interface PollExtraQuestionChoice {
+  code: string
+  label: string
+}
+
+export interface PollExtraQuestionTextOptions {
+  label: string
+  required: boolean
+  placeholder: string
+}
+
+export interface PollExtraQuestionCompoundOptions {
+  label: string
+  required: boolean
+  children: Array<PollExtraQuestion>
+}
+
+export type PollExtraQuestionOptions =
+  | PollExtraQuestionChoiceOptions
+  | PollExtraQuestionTextOptions
+  | PollExtraQuestionCompoundOptions
---
+++ src/data/mapper/PollConfigQuestionMapper.ts
@@ -0,0 +1,137 @@
+import {
+  PollExtraQuestion,
+  PollExtraQuestionOptions,
+  PollExtraQuestionPage,
+  PollExtraQuestionType,
+} from '../../core/entities/PollExtraQuestion'
+import {
+  NO_ID,
+  YES_ID,
+} from '../../screens/pollDetailUserData/PollDetailQuestionUserDataViewModelMapper'
+import i18n from '../../utils/i18n'
+import {
+  RestPollConfigQuestion,
+  RestPollConfigQuestionBooleanOptions,
+  RestPollConfigQuestionChoiceOptions,
+  RestPollConfigQuestionCompounOptions,
+  RestPollConfigQuestionOptions,
+  RestPollConfigQuestionPage,
+  RestPollConfigQuestionTextOptions,
+  RestPollConfigQuestionType,
+} from '../restObjects/RestPollConfigQuestion'
+
+export const PollConfigQuestionPageMapper = {
+  map: (restObject: RestPollConfigQuestionPage): PollExtraQuestionPage => {
+    return {
+      description: restObject.description,
+      questions: PollConfigQuestionMapper.map(restObject.questions),
+    }
+  },
+}
+
+export const PollConfigQuestionMapper = {
+  map: (restArray: Array<RestPollConfigQuestion>): Array<PollExtraQuestion> => {
+    return restArray.map((question) => {
+      const dependency = question.dependency
+        ? {
+            question: question.dependency.question,
+            choices: question.dependency.choices.map((choice) => choice),
+          }
+        : undefined
+
+      let type: string
+      switch (question.type) {
+        case 'boolean':
+        case 'choice':
+          type = 'choice'
+          break
+        case 'compound':
+          type = 'compound'
+          break
+        case 'text':
+          type = 'text'
+          break
+      }
+
+      return {
+        code: question.code,
+        type: type as PollExtraQuestionType,
+        dependency: dependency,
+        options: PollConfigQuestionOptionsMapper.map(
+          question.type,
+          question.options,
+        ),
+      }
+    })
+  },
+}
+
+export const PollConfigQuestionOptionsMapper = {
+  map: (
+    type: RestPollConfigQuestionType,
+    restObject: RestPollConfigQuestionOptions,
+  ): PollExtraQuestionOptions => {
+    switch (type) {
+      case 'boolean':
+        const booleanOptions = restObject as RestPollConfigQuestionBooleanOptions
+        return {
+          title: booleanOptions.label,
+          subtitle: booleanOptions.help,
+          required: booleanOptions.required,
+          columns: 2,
+          multiple: false,
+          choices: [
+            {
+              code: YES_ID,
+              label: i18n.t('polldetail.user_form.positive_choice'),
+            },
+            {
+              code: NO_ID,
+              label: i18n.t('polldetail.user_form.negative_choice'),
+            },
+          ],
+        }
+      case 'choice':
+        const choiceOptions = restObject as RestPollConfigQuestionChoiceOptions
+        const choices = Array.from(
+          new Map(Object.entries(choiceOptions.choices)),
+          ([name, value]) => {
+            return { code: name, label: value }
+          },
+        )
+        let columns: number
+        switch (choiceOptions.widget) {
+          case 'single_row':
+            columns = choices.length
+            break
+          case 'cols_1':
+            columns = 1
+            break
+          case 'cols_2':
+            columns = 2
+            break
+        }
+        return {
+          title: choiceOptions.label,
+          required: choiceOptions.required,
+          columns: columns,
+          multiple: choiceOptions.multiple,
+          choices: choices,
+        }
+      case 'compound':
+        const compoundOptions = restObject as RestPollConfigQuestionCompounOptions
+        return {
+          label: compoundOptions.label,
+          required: compoundOptions.required,
+          children: PollConfigQuestionMapper.map(compoundOptions.children),
+        }
+      case 'text':
+        const textOptions = restObject as RestPollConfigQuestionTextOptions
+        return {
+          label: textOptions.label,
+          required: textOptions.required,
+          placeholder: textOptions.placeholder,
+        }
+    }
+  },
+}
