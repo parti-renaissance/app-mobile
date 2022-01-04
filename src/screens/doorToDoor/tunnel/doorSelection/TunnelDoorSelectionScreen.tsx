08a1fc8b79baa5b1e38cc8a7650c0b49561fdb7d
Romain Goutte-Fangeas <romain.goutte@fabernovel.com>
2022-01-04 16:04:43 +0100

    Update door selection screen

--- src/screens/doorToDoor/tunnel/doorSelection/TunnelDoorSelectionScreen.tsx
+++ src/screens/doorToDoor/tunnel/doorSelection/TunnelDoorSelectionScreen.tsx
@@ -1,51 +1,101 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import { Image, StyleSheet, View, Text } from 'react-native'
 import SafeAreaView from 'react-native-safe-area-view'
 import { DoorToDoorTunnelStartScreenProp, Screen } from '../../../../navigation'
 import { Colors, Spacing, Typography } from '../../../../styles'
 import i18n from '../../../../utils/i18n'
 import { PrimaryButton, SecondaryButton } from '../../../shared/Buttons'
-import Swiper from 'react-native-swiper'
+import { TouchablePlatform } from '../../../shared/TouchablePlatform'
 
 const TunnelDoorSelectionScreen: FunctionComponent<DoorToDoorTunnelStartScreenProp> = ({
   navigation,
   route,
 }) => {
-  // TODO (Chawki 21.12.2021) - check the liste of campaign structure
-  const campaignId = route.params.campaignId
+  const [selectedDoor, setSelectedDoor] = useState(1)
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: i18n.t('doorToDoor.tunnel.door.shortTitle', {
+        count: route.params.buildingParams.floor,
+        buildingName: route.params.buildingParams.block,
+        floorName: route.params.buildingParams.floor,
+        door: selectedDoor,
+        context: route.params.buildingParams.floor,
+      }),
+    })
+  }, [navigation, route.params, selectedDoor])
+
+  const updateSelectedDoor = (newSelectedDoor: number) => {
+    if (newSelectedDoor >= 1) {
+      setSelectedDoor(newSelectedDoor)
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
-      <Swiper showsButtons={true} showsPagination={false}>
-        {/* TODO (Chawki 21.12.2021) - Check array structure to map the list of Swiper */}
-        <View style={styles.contentContainer}>
+      <View style={styles.contentContainer}>
+        <View style={styles.doorImageContainer}>
+          <TouchablePlatform
+            style={styles.changeDoorIndicator}
+            touchHighlight={Colors.touchHighlight}
+            onPress={() => {
+              updateSelectedDoor(selectedDoor - 1)
+            }}
+          >
+            <Image source={require('../../../../assets/images/iconBack.png')} />
+          </TouchablePlatform>
           <Image
             source={require('../../../../assets/images/papDoorIcon.png')}
           />
-          <Text style={styles.title}>__TITLE__</Text>
-          <Text style={styles.body}>
-            {i18n.t('doorToDoor.tunnel.start.rdvFirstDoor')}
-          </Text>
-          <Text style={styles.note}>
-            {i18n.t('doorToDoor.tunnel.start.doorFinished')}
-          </Text>
-          <Text style={styles.note}>
-            {i18n.t('doorToDoor.tunnel.start.buildingFinished')}
-          </Text>
+          <TouchablePlatform
+            style={styles.changeDoorIndicator}
+            touchHighlight={Colors.touchHighlight}
+            onPress={() => {
+              updateSelectedDoor(selectedDoor + 1)
+            }}
+          >
+            <Image
+              style={styles.nextDoor}
+              source={require('../../../../assets/images/iconBack.png')}
+            />
+          </TouchablePlatform>
         </View>
-      </Swiper>
+        <Text style={styles.title}>
+          {i18n.t('doorToDoor.tunnel.door.title', {
+            count: route.params.buildingParams.floor,
+            buildingName: route.params.buildingParams.block,
+            floorName: route.params.buildingParams.floor,
+            context: route.params.buildingParams.floor,
+          })}
+        </Text>
+        <Text style={styles.body}>
+          {i18n.t('doorToDoor.tunnel.door.rdvDoor', {
+            count: selectedDoor,
+            door: selectedDoor,
+          })}
+        </Text>
+        <Text style={styles.note}>
+          {i18n.t('doorToDoor.tunnel.door.doorFinished')}
+        </Text>
+        <Text style={styles.note}>
+          {i18n.t('doorToDoor.tunnel.door.buildingFinished')}
+        </Text>
+      </View>
       <View style={styles.bottomContainer}>
         <PrimaryButton
-          title={i18n.t('doorToDoor.tunnel.start.doorknocked')}
+          title={i18n.t('doorToDoor.tunnel.door.doorknocked')}
           onPress={() =>
             navigation.navigate(Screen.tunnelDoorOpening, {
-              campaignId: campaignId,
-              buildingParams: route.params.buildingParams,
+              campaignId: route.params.campaignId,
+              buildingParams: {
+                ...route.params.buildingParams,
+                door: selectedDoor,
+              },
             })
           }
         />
         <SecondaryButton
-          title={i18n.t('doorToDoor.tunnel.start.floorFinished')}
+          title={i18n.t('doorToDoor.tunnel.door.floorFinished')}
           style={styles.finished}
           onPress={() => {}}
         />
@@ -65,6 +115,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: Spacing.margin,
     paddingTop: Spacing.margin,
   },
+  changeDoorIndicator: {
+    borderRadius: 32,
+    padding: Spacing.unit,
+  },
   container: {
     backgroundColor: Colors.defaultBackground,
     flex: 1,
@@ -75,8 +129,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: Spacing.margin,
   },
+  doorImageContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
   finished: {
     marginTop: Spacing.unit,
+  },
+  nextDoor: {
+    transform: [{ rotate: '180deg' }],
   },
   note: {
     ...Typography.footnoteLight,
