991e963cb2069cfa24846229a59229602b871df4
Romain Goutte-Fangeas <romain.goutte@fabernovel.com>
2022-01-26 15:57:33 +0100

    Send poll answers in any case and manage error to enqueue if needed

--- src/core/interactor/SendDoorPollAnswersInteractor.ts
+++ src/core/interactor/SendDoorPollAnswersInteractor.ts
@@ -1,12 +1,12 @@
 import DoorToDoorRepository from '../../data/DoorToDoorRepository'
 import { DoorToDoorPollResult } from '../../screens/doorToDoor/tunnel/survey/DoorToDoorQuestionResult'
 import { BuildingType } from '../entities/DoorToDoor'
-import NetInfo from '@react-native-community/netinfo'
 import { DoorToDoorPollParams } from '../entities/DoorToDoorPollParams'
 import {
   SendDoorToDoorPollAnswersJobQueue,
   SendDoorToDoorPollAnswersJobQueueItem,
 } from '../../data/store/SendDoorToDoorPollAnswersJobQueue'
+import { ServerTimeoutError } from '../errors'
 
 export const INTERLOCUTOR_ACCEPT_TO_ANSWER_CODE = 'accept_to_answer'
 
@@ -16,23 +16,26 @@ export class SendDoorPollAnswersInteractor {
   public async execute(
     params: SendDoorToDoorPollAnswersJobQueueItem,
   ): Promise<void> {
-    const networkState = await NetInfo.fetch()
-    if (networkState.isInternetReachable) {
-      const pollParams = {
-        campaignId: params.campaignId,
-        buildingId: params.buildingParams.id,
-        status: params.doorStatus,
-        block: params.buildingParams.block,
-        floor: params.buildingParams.floor,
-        door: params.buildingParams.door,
-      }
-      this.sendAnswers(
+    const pollParams = {
+      campaignId: params.campaignId,
+      buildingId: params.buildingParams.id,
+      status: params.doorStatus,
+      block: params.buildingParams.block,
+      floor: params.buildingParams.floor,
+      door: params.buildingParams.door,
+    }
+    try {
+      await this.sendAnswers(
         params.doorStatus,
         pollParams,
         params.pollResult ?? { answers: [], qualificationAnswers: [] },
       )
-    } else {
-      this.enqueueAnswers(params)
+    } catch (error) {
+      if (error instanceof ServerTimeoutError) {
+        this.enqueueAnswers(params)
+        return
+      }
+      throw error
     }
 
     if (params.buildingParams.type === 'house') {
