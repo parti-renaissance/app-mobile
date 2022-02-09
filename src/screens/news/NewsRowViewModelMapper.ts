c9953ea6b648ea66ae24d61756f0dd0765279159
Pierre Felgines <pierre.felgines@fabernovel.com>
2022-02-09 09:53:37 +0100

    Update news date format

--- src/screens/news/NewsRowViewModelMapper.ts
+++ src/screens/news/NewsRowViewModelMapper.ts
@@ -1,14 +1,16 @@
 import { News } from '../../core/entities/News'
-import { format } from 'date-fns'
 import i18n from '../../utils/i18n'
 import { NewsRowViewModel } from './NewsRowViewModel'
+import { formatLocalizedDate } from '../../utils/DateFormatter'
 
 export const NewsRowViewModelMapper = {
   map: (news: News): NewsRowViewModel => {
     return {
       id: news.id,
       title: news.title,
-      date: format(news.date, i18n.t('home.news.date_format')),
+      date: i18n.t('home.news.date_format', {
+        date: formatLocalizedDate(news.date, i18n.t('home.news.date_pattern')),
+      }),
       isEnabled: news.url !== undefined,
     }
   },
