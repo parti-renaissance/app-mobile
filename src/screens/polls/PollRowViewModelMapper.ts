8ad0f681188ffa01756319ce59afa561cba1d23a
Pierre Felgines <pierre.felgines@fabernovel.com>
2022-03-11 16:22:14 +0100

    Update poll images

--- src/screens/polls/PollRowViewModelMapper.ts
+++ src/screens/polls/PollRowViewModelMapper.ts
@@ -5,10 +5,17 @@ import { PollRowViewModel } from './PollRowViewModel'
 export const PollRowViewModelMapper = {
   map: (poll: Poll): PollRowViewModel => {
     const pollImages = [
-      require('../../assets/images/blue/imageSondage01.png'),
-      require('../../assets/images/blue/imageSondage02.png'),
-      require('../../assets/images/blue/imageSondage03.png'),
-      require('../../assets/images/blue/imageSondage04.png'),
+      require('../../assets/images/pollImage01.jpg'),
+      require('../../assets/images/pollImage02.jpg'),
+      require('../../assets/images/pollImage03.jpg'),
+      require('../../assets/images/pollImage04.jpg'),
+      require('../../assets/images/pollImage05.jpg'),
+      require('../../assets/images/pollImage06.jpg'),
+      require('../../assets/images/pollImage07.jpg'),
+      require('../../assets/images/pollImage08.jpg'),
+      require('../../assets/images/pollImage09.jpg'),
+      require('../../assets/images/pollImage10.jpg'),
+      require('../../assets/images/pollImage11.jpg'),
     ]
     const pollImage = pollImages[poll.id % pollImages.length]
     return {
