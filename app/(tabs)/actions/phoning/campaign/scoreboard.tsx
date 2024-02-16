7d868792a3840ef50d2235ab69fe05b0875340bd
Antonin Carlin <antonin.carlin@icloud.com>
2024-02-16 11:03:32 +0100

    feat: scoreboard screen

--- app/(tabs)/actions/phoning/campaign/scoreboard.tsx
+++ app/(tabs)/actions/phoning/campaign/scoreboard.tsx
@@ -1,2 +1,46 @@
-import PhoningCampaignScoreboardScreen from "@/screens/phoningCampaignScoreboard/PhoningCampaignScoreboardScreen";
-export default PhoningCampaignScoreboardScreen;
+import React, { FunctionComponent, useEffect } from 'react'
+import { StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { ActionsNavigatorScreenProps } from '@/navigation/actions/ActionsNavigatorScreenProps'
+import { Colors, Spacing } from '@/styles'
+import i18n from '@/utils/i18n'
+import { PhoningCampaignRankingView } from '@/screens/shared/PhoningCampaignRankingView'
+import { PhoningScoreboardRowViewModelMapper } from '@/screens/phoningCampaignScoreboard/PhoningScoreboardRowViewModelMapper'
+import {router, useLocalSearchParams, useNavigation} from 'expo-router'
+import{ useCampaignStore } from '@/data/store/phoning'
+
+type PhoningCampaignScoreboardScreenProps =
+    ActionsNavigatorScreenProps<'PhoningCampaignScoreboard'>
+
+const PhoningCampaignScoreboardScreen: FunctionComponent<
+    PhoningCampaignScoreboardScreenProps
+> = () => {
+    const navigation = useNavigation()
+    const { campaign } = useCampaignStore()
+    
+    useEffect(() => {
+        navigation.setOptions({
+            title: i18n.t('phoning.scoreboard.title'),
+        })
+    }, [navigation])
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <PhoningCampaignRankingView
+                viewModel={PhoningScoreboardRowViewModelMapper.map(
+                    campaign.scoreboard,
+                )}
+            />
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: Colors.defaultBackground,
+        flex: 1,
+        padding: Spacing.margin,
+    },
+})
+
+export default PhoningCampaignScoreboardScreen
