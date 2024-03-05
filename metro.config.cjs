fbae4575a7c002492a8d6ae0431187bbd41a38c5
Antonin <antonin.carlin@icloud.com>
2024-03-05 09:25:12 +0100

    feat: init tamagui

--- metro.config.cjs
+++ metro.config.cjs
@@ -2,6 +2,15 @@
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getSentryExpoConfig(__dirname);
+const config = getSentryExpoConfig(__dirname, {
+    // [Web-only]: Enables CSS support in Metro.
+    isCSSEnabled: false,
+});
 
-module.exports = config;
+// 2. Enable Tamagui
+const { withTamagui } = require('@tamagui/metro-plugin')
+module.exports = withTamagui(config, {
+  components: ['tamagui'],
+  config: './tamagui.config.ts',
+  outputCSS: './tamagui-web.css',
+})
