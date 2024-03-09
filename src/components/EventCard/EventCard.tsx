21df870268c0a011eb0f2d4aa37687ac5f1ffc1e
Antonin <antonin.carlin@icloud.com>
2024-03-09 21:51:48 +0100

    feat(EventCard): add lcoation and btn

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,4 +1,5 @@
 import VoxCard from '@/components/VoxCard/VoxCard'
+import { XStack, Button, Text } from 'tamagui'
 
 export interface EventVoxCardProps {
   payload: {
@@ -20,10 +21,18 @@ const EventCard = ({payload}: EventVoxCardProps) => {
   const chipVariant = { [`${payload.type}`]: true }
   return (
     <VoxCard>
-      {/* @ts-ignore  */}
       <VoxCard.Chip {...chipVariant}>{payload.tag}</VoxCard.Chip>
       <VoxCard.Title>{payload.title}</VoxCard.Title>
       <VoxCard.Date date={payload.date}/>
+      <VoxCard.Location location={payload.location}/>
+      <XStack justifyContent="space-between">
+        <Button size="$2.5" backgroundColor="$white1" borderWidth="$1" paddingHorizontal="$4" borderColor="$gray3">
+          <Text fontFamily="$PublicSans" fontWeight="$6" color="$gray8">Voir l'événement</Text>
+        </Button>
+        <Button size="$2.5" backgroundColor="$gray8" paddingHorizontal="$4">
+          <Text fontFamily="$PublicSans" fontWeight="$6" color="$white1">M'inscrire</Text>
+        </Button>
+      </XStack>
     </VoxCard>
   )
 }
