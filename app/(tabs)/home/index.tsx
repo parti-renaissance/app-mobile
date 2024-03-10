73bd9e02096ba930dd7e8e5932ab47f0731dd9fb
Antonin <antonin.carlin@icloud.com>
2024-03-10 22:10:05 +0100

    feat: navbar

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,5 +1,6 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
 import BoundarySuspenseWrapper from '@/components/BoundarySuspenseWrapper'
+import Navbar from '@/components/Navbar/Navbar'
 import HomeFeedList from '@/screens/home/feed/HomeFeedList'
 import { Stack } from 'expo-router'
 import { Avatar, getToken, YStack } from 'tamagui'
@@ -8,24 +9,10 @@ const HomeScreen: React.FC = () => (
   <>
     <Stack.Screen
       options={{
-        header: () => (
-          <SafeAreaView
-            edges={['top']}
-            style={{
-              backgroundColor: getToken('$white1'),
-            }}
-          >
-            <YStack justifyContent="center" p="$4.5">
-              <Avatar circular size="$4">
-                <Avatar.Image source={{ uri: 'https://picsum.photos/200/200', width: 200, height: 200 }} />
-                <Avatar.Fallback bc="$gray3" />
-              </Avatar>
-            </YStack>
-          </SafeAreaView>
-        ),
+        header: () => <Navbar />,
       }}
     />
-    <YStack>
+    <YStack flex={1}>
       <BoundarySuspenseWrapper loadingMessage="Nous chargons votre fil">
         <HomeFeedList />
       </BoundarySuspenseWrapper>
