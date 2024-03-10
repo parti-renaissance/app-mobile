61736b5def9ba9405a7936046342d90a1dda3550
Antonin <antonin.carlin@icloud.com>
2024-03-10 10:42:33 +0100

    feat(EventCard): subscribe state

--- src/components/EventCard/EventCard.stories.tsx
+++ src/components/EventCard/EventCard.stories.tsx
@@ -5,25 +5,44 @@ export default {
   component: EventCard,
 };
 
-export const Event = {
+const props = {
+  payload: {
+    title: 'Titre long : Lorem ipsum dolor sit amet consectetur ipsum dolor sit amet consectetur',
+    type: 'event',
+    tag: 'Moment de convivialité',
+    image: 'https://picsum.photos/600/244',
+    date: new Date(),
+    location: {
+      city: "Montpellier",
+      postalCode: "34090",
+      street: " 43 Place du marché de la croix saint Simon",
+    },
+    author: {
+      role: 'Assemblée départementale de Paris',
+      name: 'Jean Voulzi',
+      title: 'Responsable mobilisation',
+      pictureLink: 'https://picsum.photos/id/64/200/200',
+    },
+  }
+}
+
+export const Event = { args: props };
+
+
+export const EventNoImage = {
   args: {
     payload: {
-      title: 'Titre long : Lorem ipsum dolor sit amet consectetur ipsum dolor sit amet consectetur',
-      type: 'event',
-      tag: 'Moment de convivialité',
-      image: 'https://picsum.photos/600/244',
-      date: new Date(),
-      location: {
-        city: "Montpellier",
-        postalCode: "34090",
-        street: " 43 Place du marché de la croix saint Simon",
-      },
-      author: {
-        role: 'Assemblée départementale de Paris',
-        name: 'Jean Voulzi',
-        title: 'Responsable mobilisation',
-        pictureLink: 'https://picsum.photos/id/64/200/200',
-      },
+      ...props.payload,
+      image: undefined,
+    }
+  }
+};
+
+export const EventSubscribed = {
+  args: {
+    payload: {
+      ...props.payload,
+      isSubscribed: true,
     }
   }
 };
--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,33 +1,43 @@
-import VoxCard, {VoxCardAuthorProps, VoxCardDateProps, VoxCardLocationProps } from '@/components/VoxCard/VoxCard'
+import VoxCard, { VoxCardAuthorProps, VoxCardDateProps, VoxCardLocationProps } from '@/components/VoxCard/VoxCard'
 import { XStack, Button, Text } from 'tamagui'
 
 export interface EventVoxCardProps {
+  onSubscribe?: () => void,
+  onShowEvent?: () => void,
   payload: {
     title: string,
     tag: string,
     type: 'news' | 'event' | 'riposte' | 'action',
     image?: string,
+    isSubscribed: boolean,
   } & VoxCardDateProps & VoxCardLocationProps & VoxCardAuthorProps
 }
 
 
-const EventCard = ({payload}: EventVoxCardProps) => {
+const EventCard = ({ payload, onSubscribe, onShowEvent }: EventVoxCardProps) => {
   const chipVariant = { [`${payload.type}`]: true }
   return (
     <VoxCard>
       <VoxCard.Chip {...chipVariant}>{payload.tag}</VoxCard.Chip>
       <VoxCard.Title>{payload.title}</VoxCard.Title>
-      {payload.image && <VoxCard.Image image={payload.image}/> }
-      <VoxCard.Date date={payload.date}/>
-      <VoxCard.Location location={payload.location}/>
-      <VoxCard.Author author={payload.author}/>
+      {payload.image && <VoxCard.Image image={payload.image} />}
+      <VoxCard.Date date={payload.date} />
+      <VoxCard.Location location={payload.location} />
+      <VoxCard.Author author={payload.author} />
       <XStack justifyContent="space-between">
-        <Button size="$2.5" backgroundColor="$white1" borderWidth="$1" paddingHorizontal="$4" borderColor="$gray3">
+        <Button size="$2.5" backgroundColor="$white1" borderWidth="$1" paddingHorizontal="$4" borderColor="$gray3" onPress={onShowEvent}>
           <Text fontFamily="$PublicSans" fontWeight="$6" color="$gray8">Voir l'événement</Text>
         </Button>
-        <Button size="$2.5" backgroundColor="$gray8" paddingHorizontal="$4">
-          <Text fontFamily="$PublicSans" fontWeight="$6" color="$white1">M'inscrire</Text>
-        </Button>
+        {payload.isSubscribed ? (
+          <Button size="$2.5" chromeless paddingHorizontal="$4" onPress={onSubscribe}>
+            <Text fontFamily="$PublicSans" fontWeight="$6" color="$blue6">Inscrit(e)</Text>
+          </Button>
+
+        ) : (
+          <Button size="$2.5" backgroundColor="$gray8" paddingHorizontal="$4" onPress={onSubscribe}>
+            <Text fontFamily="$PublicSans" fontWeight="$6" color="$white1">M'inscrire</Text>
+          </Button>
+        )}
       </XStack>
     </VoxCard>
   )
