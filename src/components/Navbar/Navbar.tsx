afee344f5df85c5fffb812ed01e6a965edc3ccdb
Brandon Sueur <gynidark@gmail.com>
2024-03-12 00:48:25 +0100

    refactor: navbar web and header mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,25 +1,101 @@
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react'
+import EuCampaignIllustration from '@/assets/illustrations/EuCampaignIllustration'
 import { useGetProfilObserver } from '@/hooks/useProfil'
-import { Avatar, getToken, YStack } from 'tamagui'
+import { Analytics, AnalyticsScreens } from '@/utils/Analytics'
+import { ROUTES } from 'app/(tabs)/_layout'
+import { Link, usePathname } from 'expo-router'
+import { Avatar, Button, Stack, styled, Text, useMedia, View } from 'tamagui'
 
-const Navbar: React.FC = () => {
+const opacityToHexCode = (hex: string, opacity: number) => {
+  const opacityHex = Math.round(opacity * 255).toString(16)
+  return `${hex}${opacityHex}`
+}
+
+const ButtonNav = styled(Button, {
+  backgroundColor: 'transparent',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: 2,
+})
+
+const NavBar: React.FC = () => {
+  const pathname = usePathname()
+  const media = useMedia()
+
   const { data: profile } = useGetProfilObserver()
-  console.log(profile)
+
   return (
-    <SafeAreaView
-      edges={['top']}
-      style={{
-        backgroundColor: getToken('$white1'),
-      }}
+    <Stack
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      backgroundColor="white"
+      borderBottomWidth={2}
+      borderBottomColor="rgba(145, 158, 171, 0.32)"
+      height={82}
+      paddingHorizontal={'$4'}
     >
-      <YStack justifyContent="center" p="$4.5">
+      <EuCampaignIllustration />
+      <Stack flexDirection="row" gap={4}>
+        {ROUTES.map((route) => {
+          const focused = pathname.replace('/', '') === route.name
+          const colorOpacity = opacityToHexCode(route.gradiant[0], 0.09)
+          const Icon = () => <route.icon size={28} color={focused ? route.gradiant[0] : '#637381'} />
+
+          return (
+            <Link
+              key={route.name}
+              href={`/${route.name}` as never}
+              onPress={() => {
+                Analytics.logNavBarItemSelected(route.screenName as AnalyticsScreens)
+              }}
+            >
+              <ButtonNav
+                hoverStyle={{
+                  backgroundColor: !focused ? colorOpacity : 'transparent',
+                  borderColor: 'transparent',
+                }}
+                pressStyle={{
+                  backgroundColor: 'transparent',
+                  borderColor: 'transparent',
+                }}
+              >
+                <Button.Icon scaleIcon={2}>
+                  <Icon />
+                </Button.Icon>
+
+                {media.gtMd && (
+                  <Button.Text color={focused ? route.gradiant[0] : 'gray8'} fontWeight={'500'}>
+                    {route.screenName}
+                  </Button.Text>
+                )}
+              </ButtonNav>
+            </Link>
+          )
+        })}
+      </Stack>
+
+      <View flexDirection="row" gap={'$4'} justifyContent="space-between" alignItems="center">
+        <Stack gap={2} flexDirection="column" alignContent="flex-end" alignItems="flex-end">
+          <Text fontFamily={'$PublicSans'} color={'gray8'} fontWeight={'500'}>
+            {profile?.first_name} {profile?.last_name}
+          </Text>
+
+          {/* TODO: add the personal code */}
+          <Text fontFamily={'$PublicSans'} fontSize={12} color={'gray5'}>
+            #000000
+          </Text>
+        </Stack>
+
         <Avatar circular size="$4">
+          {/* TODO: add the real avatar*/}
           <Avatar.Image source={{ uri: 'https://picsum.photos/200/200', width: 200, height: 200 }} />
           <Avatar.Fallback bc="$gray3" />
         </Avatar>
-      </YStack>
-    </SafeAreaView>
+      </View>
+    </Stack>
   )
 }
 
-export default Navbar
+export default NavBar
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
 import Button from './Button'
+import Chip from './Chip'
+import Header from './Header'
+import NavBar from './Navbar'
 
-export { Button }
+export { Button, NavBar, Chip, Header }
