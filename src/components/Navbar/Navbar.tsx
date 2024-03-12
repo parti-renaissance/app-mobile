ec42d1b1419a8893ee82d2a702c3b5ba44872741
Brandon Sueur <gynidark@gmail.com>
2024-03-12 03:27:46 +0100

    feat: navbar not logged

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,6 +5,7 @@ import { Analytics, AnalyticsScreens } from '@/utils/Analytics'
 import { ROUTES } from 'app/routes'
 import { Link, usePathname } from 'expo-router'
 import { Avatar, Button, Stack, styled, Text, useMedia, View } from 'tamagui'
+import ButtonCustom from '../Button'
 
 const opacityToHexCode = (hex: string, opacity: number) => {
   const opacityHex = Math.round(opacity * 255).toString(16)
@@ -40,62 +41,84 @@ const NavBar: React.FC = () => {
         <EuCampaignIllustration />
       </Link>
 
-      <Stack flexDirection="row" gap={4}>
-        {ROUTES.map((route) => {
-          const focused = pathname.replace('/', '') === route.name
-          const colorOpacity = opacityToHexCode(route.gradiant[0], 0.09)
-          const Icon = () => <route.icon size={28} color={focused ? route.gradiant[1] : '#637381'} />
+      {profile && (
+        <Stack flexDirection="row" gap={4}>
+          {ROUTES.map((route) => {
+            const focused = pathname.replace('/', '') === route.name
+            const colorOpacity = opacityToHexCode(route.gradiant[0], 0.09)
+            const Icon = () => <route.icon size={28} color={focused ? route.gradiant[1] : '#637381'} />
 
-          return (
-            <Link
-              key={route.name}
-              href={`/${route.name}` as never}
-              onPress={() => {
-                Analytics.logNavBarItemSelected(route.screenName as AnalyticsScreens)
-              }}
-            >
-              <ButtonNav
-                hoverStyle={{
-                  backgroundColor: !focused ? colorOpacity : 'transparent',
-                  borderColor: 'transparent',
-                }}
-                pressStyle={{
-                  backgroundColor: 'transparent',
-                  borderColor: 'transparent',
+            return (
+              <Link
+                key={route.name}
+                href={`/${route.name}` as never}
+                onPress={() => {
+                  Analytics.logNavBarItemSelected(route.screenName as AnalyticsScreens)
                 }}
               >
-                <Button.Icon scaleIcon={2}>
-                  <Icon />
-                </Button.Icon>
+                <ButtonNav
+                  hoverStyle={{
+                    backgroundColor: !focused ? colorOpacity : 'transparent',
+                    borderColor: 'transparent',
+                  }}
+                  pressStyle={{
+                    backgroundColor: 'transparent',
+                    borderColor: 'transparent',
+                  }}
+                >
+                  <Button.Icon scaleIcon={2}>
+                    <Icon />
+                  </Button.Icon>
 
-                {media.gtMd && (
-                  <Button.Text color={focused ? route.gradiant[1] : 'gray8'} fontWeight={'500'}>
-                    {route.screenName}
-                  </Button.Text>
-                )}
-              </ButtonNav>
-            </Link>
-          )
-        })}
-      </Stack>
+                  {media.gtMd && (
+                    <Button.Text color={focused ? route.gradiant[1] : 'gray8'} fontWeight={'500'}>
+                      {route.screenName}
+                    </Button.Text>
+                  )}
+                </ButtonNav>
+              </Link>
+            )
+          })}
+        </Stack>
+      )}
 
       <View flexDirection="row" gap={'$4'} justifyContent="space-between" alignItems="center">
-        <Stack gap={4} flexDirection="column" alignContent="flex-end" alignItems="flex-end">
-          <Text fontFamily={'$PublicSans'} color={'gray8'} fontWeight={'500'}>
-            {profile?.first_name} {profile?.last_name}
-          </Text>
+        {profile ? (
+          <>
+            <Stack gap={4} flexDirection="column" alignContent="flex-end" alignItems="flex-end">
+              <Text fontFamily={'$PublicSans'} color={'gray8'} fontWeight={'500'}>
+                {profile?.first_name} {profile?.last_name}
+              </Text>
 
-          {/* TODO: add the personal code */}
-          <Text fontFamily={'$PublicSans'} fontSize={12} color={'$gray5'}>
-            #000000
-          </Text>
-        </Stack>
+              {/* TODO: add the personal code */}
+              <Text fontFamily={'$PublicSans'} fontSize={12} color={'$gray5'}>
+                #000000
+              </Text>
+            </Stack>
 
-        <Avatar circular size="$4">
-          {/* TODO: add the real avatar*/}
-          <Avatar.Image source={{ uri: 'https://picsum.photos/200/200', width: 200, height: 200 }} />
-          <Avatar.Fallback bc="$gray3" />
-        </Avatar>
+            <Avatar circular size="$4">
+              {/* TODO: add the real avatar*/}
+              <Avatar.Image source={{ uri: 'https://picsum.photos/200/200', width: 200, height: 200 }} />
+              <Avatar.Fallback bc="$gray3" />
+            </Avatar>
+          </>
+        ) : (
+          <>
+            <Stack gap={'$2'} flexDirection="row">
+              <ButtonCustom variant="text" height={'$3'}>
+                <ButtonCustom.Text color={'gray8'} fontWeight={'800'}>
+                  Me connecter
+                </ButtonCustom.Text>
+              </ButtonCustom>
+
+              <ButtonCustom backgroundColor={'$blue6'} height={'$3'} borderRadius={'$3'} padding={'$3'}>
+                <ButtonCustom.Text color={'white'} fontWeight={'800'}>
+                  J'adhère
+                </ButtonCustom.Text>
+              </ButtonCustom>
+            </Stack>
+          </>
+        )}
       </View>
     </Stack>
   )
