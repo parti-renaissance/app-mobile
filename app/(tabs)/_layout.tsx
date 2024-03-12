e24609bf4f8b8925704ef3f989e2433121d63e45
Brandon Sueur <gynidark@gmail.com>
2024-03-12 02:54:24 +0100

    fix: fonts and circle import ROUTES

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,54 +2,14 @@ import React from 'react'
 import { Platform, Pressable } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import NavBar from '@/components/Navbar/Navbar'
+import { ROUTES } from '@/navigation/Navigator'
 import { Analytics, AnalyticsScreens } from '@/utils/Analytics'
 import type { IconProps } from '@tamagui/helpers-icon'
-import { Calendar as CalendarSvg, Home as HomeSvg, Inbox as InboxSvg, Sparkle as SparkleSvg, Zap as ZapSvg } from '@tamagui/lucide-icons'
 import { Stack as StackRouter, Tabs, usePathname } from 'expo-router'
 import { View } from 'tamagui'
 
 const IS_WEB = Platform.OS === 'web'
 const TAB_BAR_HEIGTH = 60
-
-type TabRoute = {
-  name: string
-  screenName: string
-  icon: React.NamedExoticComponent<IconProps>
-  gradiant?: string[]
-}
-
-export const ROUTES: TabRoute[] = [
-  {
-    name: 'home',
-    screenName: 'Fil',
-    icon: HomeSvg,
-    gradiant: ['#8D98FF', '#8050E6'],
-  },
-  {
-    name: 'news',
-    screenName: 'Événements',
-    icon: CalendarSvg,
-    gradiant: ['#52ABFB', '#0868E7'],
-  },
-  {
-    name: 'actions',
-    screenName: 'Actions',
-    icon: ZapSvg,
-    gradiant: ['#68F984', '#06B827'],
-  },
-  {
-    name: 'events',
-    screenName: 'Ripostes',
-    icon: SparkleSvg,
-    gradiant: ['#FDA302', '#F7681E'],
-  },
-  {
-    name: 'tools',
-    screenName: 'Ressources',
-    icon: InboxSvg,
-    gradiant: ['#E461E8', '#8B2DBF'],
-  },
-]
 
 const getScreenname = (route: string): AnalyticsScreens => {
   const tabRoute = ROUTES.find((tabRoute) => tabRoute.name === route)
