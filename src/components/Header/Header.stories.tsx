0ad32704c63fcc04aede2e7a60a10faf4d1189d0
Brandon Sueur <gynidark@gmail.com>
2024-03-12 04:31:10 +0100

    feat: new component Header mobile

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,26 @@
+import Header from './Header'
+
+export default {
+  title: 'Header',
+  component: Header,
+}
+
+export function Default() {
+  return <Header onClosePress={() => {}} onSearchPress={() => {}} />
+}
+
+export function WithButtonSearch() {
+  return <Header onSearchPress={() => {}} />
+}
+
+export function WithButtonClose() {
+  return <Header onClosePress={() => {}} />
+}
+
+export function WithoutLogo() {
+  return <Header hideLogo />
+}
+
+export function WithoutAvatar() {
+  return <Header hideAvatar />
+}
