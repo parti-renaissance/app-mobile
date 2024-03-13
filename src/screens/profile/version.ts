0bfdc1bb812cd958e594ad160b3e7dab8714b295
Antonin <antonin.carlin@icloud.com>
2024-03-13 16:28:53 +0100

    fix: app version

--- src/screens/profile/version.ts
+++ src/screens/profile/version.ts
@@ -1,6 +1,6 @@
-import * as deviceInfoModule from 'expo-device'
+import Constants from "expo-constants"
 import i18n from '../../utils/i18n'
 
 export const versionLabel = i18n.t('profile.version', {
-  version: deviceInfoModule.osVersion
+  version: Constants.manifest2.runtimeVersion,
 })
