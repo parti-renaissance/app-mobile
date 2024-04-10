622ce69c89c33bd5dbf59970707b6ec4f8d0eff9
Antonin <antonin.carlin@gmail.com>
2024-04-10 12:21:05 +0200

    feat: universal link (#690)

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,14 @@
-import { ConfigContext, ExpoConfig } from 'expo/config'
-import FontLib from './assets/fonts/generated-lib-fonts'
+import { ConfigContext, ExpoConfig } from 'expo/config';
+import FontLib from './assets/fonts/generated-lib-fonts';
+
 
 const baseIdentifier = 'fr.en-marche.jecoute'
 const basePackage = 'fr.en_marche.jecoute'
+
+const setAssociatedDomain = (config: Partial<ExpoConfig>, associatedDomain: string) => {
+  config.ios.associatedDomains = [`applinks:${associatedDomain}`, `webcredentials:${associatedDomain}`, `activitycontinuation:${associatedDomain}`]
+  config.android.intentFilters[0].data[0].host = associatedDomain
+}
 
 export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
   config.plugins.push([
@@ -13,6 +19,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
   ])
 
   const profile = process.env.EAS_BUILD_PROFILE
+
   if (!profile) {
     config.name = 'Vox Dev'
     config.ios.bundleIdentifier = `${baseIdentifier}.staging`
@@ -22,12 +29,14 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_ANDROID_GOOGLE_API_KEY_STAGING
     config.android.adaptiveIcon.foregroundImage = './assets/developement/adaptive-icon.jpg'
     config.ios.icon = './assets/developement/icon.jpg'
+    setAssociatedDomain(config, process.env.ASSOCIATED_DOMAIN_DEVELOPMENT)
   } else if (profile === 'production') {
     config.ios.bundleIdentifier = baseIdentifier
     config.android.package = basePackage
     config.ios.googleServicesFile = process.env.GOOGLE_SERVICES_IOS_PATH_PRODUCTION
     config.android.googleServicesFile = process.env.GOOGLE_SERVICES_ANDROID_PATH_PRODUCTION
     config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_ANDROID_GOOGLE_API_KEY_PRODUCTION
+    setAssociatedDomain(config, process.env.ASSOCIATED_DOMAIN_PRODUCTION)
   } else if (profile === 'development') {
     config.name = 'Vox Dev'
     config.ios.bundleIdentifier = `${baseIdentifier}.${profile}`
@@ -37,6 +46,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     config.ios.googleServicesFile = process.env.GOOGLE_SERVICES_IOS_PATH_DEVELOPMENT
     config.android.googleServicesFile = process.env.GOOGLE_SERVICES_ANDROID_PATH_DEVELOPMENT
     config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_ANDROID_GOOGLE_API_KEY_STAGING
+    setAssociatedDomain(config, process.env.ASSOCIATED_DOMAIN_DEVELOPMENT)
   } else {
     config.name = 'Vox Staging'
     config.android.adaptiveIcon.foregroundImage = './assets/staging/adaptive-icon.jpg'
@@ -46,6 +56,7 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     config.ios.googleServicesFile = process.env.GOOGLE_SERVICES_IOS_PATH_STAGING
     config.android.googleServicesFile = process.env.GOOGLE_SERVICES_ANDROID_PATH_STAGING
     config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_ANDROID_GOOGLE_API_KEY_STAGING
+    setAssociatedDomain(config, process.env.ASSOCIATED_DOMAIN_STAGING)
   }
 
   config.extra.storybookEnabled = process.env.STORYBOOK_ENABLED
