bc8e3ee3c0e1c5716d34dff20726f8c015e1fcc7
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-11 15:13:48 +0200

    feature(ci): #RE-1662 Revert babel config and ad bun lockfile to gitignore

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,6 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        '@tamagui/babel-plugin',
-        {
-          components: ['tamagui'],
-          config: './tamagui.config.ts',
-          logTimings: true,
-          disableExtraction: process.env.NODE_ENV === 'development',
-        },
-      ],
-    ],
+    presets: ['babel-preset-expo']
   }
 }
