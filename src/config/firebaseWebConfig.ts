63d694da8a0a3c9e3009704a435e8134f854000b
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-11 16:04:37 +0200

    RE-1662 deploy process (#689)
    * feature(ci): #RE-1662 web & mobile CI
    
    * feature(ci): #RE-1662 Revert babel config and ad bun lockfile to gitignore
    
    * feature(ci): #RE-1662 Remove unused expire
    
    * feature(ci): #RE-1662 Add env to sentry init

--- src/config/firebaseWebConfig.ts
+++ src/config/firebaseWebConfig.ts
@@ -1,10 +1,8 @@
-import * as envs from './env'
-
 export default {
-  apiKey: envs.FB_API_KEY,
-  authDomain: `${envs.FB_PROJECT_ID}.firebaseapp.com`,
-  projectId: envs.FB_PROJECT_ID,
-  storageBucket: `${envs.FB_PROJECT_ID}.appspot.com`,
-  messagingSenderId: envs.FB_SENDER_ID,
-  appId: envs.FB_APP_ID,
+  apiKey: process.env.EXPO_PUBLIC_FB_API_KEY,
+  authDomain: `${process.env.EXPO_PUBLIC_FB_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.EXPO_PUBLIC_FB_PROJECT_ID,
+  storageBucket: `${process.env.EXPO_PUBLIC_FB_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.EXPO_PUBLIC_FB_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FB_MEASUREMENT_ID,
 }
