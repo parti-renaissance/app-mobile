68e667851ebd7c46e09746ed5b5a9724a558ec97
Antonin <antonin.carlin@icloud.com>
2024-04-11 10:49:16 +0200

    fix(event): header and padding mobile

--- app/(tabs)/events/_layout.tsx
+++ app/(tabs)/events/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router'
 
 export default function AppLayout() {
   return (
-    <Stack screenOptions={headerBlank}>
+    <Stack screenOptions={{ headerShown: false}}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="[mode]/filters" options={{ headerShown: false, presentation: 'modal' }} />
     </Stack>
