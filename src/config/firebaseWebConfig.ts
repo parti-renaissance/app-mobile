b4e68d6a257be28fd29dfa8a06ad14a4f67eb2ce
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-12 11:32:39 +0200

    fix(firebase): fix wrong keys in firebaseWebConfig.ts

--- src/config/firebaseWebConfig.ts
+++ src/config/firebaseWebConfig.ts
@@ -4,5 +4,6 @@ export default {
   projectId: process.env.EXPO_PUBLIC_FB_PROJECT_ID,
   storageBucket: `${process.env.EXPO_PUBLIC_FB_PROJECT_ID}.appspot.com`,
   messagingSenderId: process.env.EXPO_PUBLIC_FB_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FB_MEASUREMENT_ID,
+  appId: process.env.EXPO_PUBLIC_FB_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FB_MEASUREMENT_ID,
 }
