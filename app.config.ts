901bbbaecb608d2f49fc2a9e6d108506192802e4
Antonin <antonin.carlin@icloud.com>
2024-04-16 11:35:44 +0200

    ci: fix envs

--- app.config.ts
+++ app.config.ts
@@ -19,6 +19,8 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
   ])
 
   const profile = process.env.EAS_BUILD_PROFILE
+  setAssociatedDomain(config, process.env.EXPO_PUBLIC_ASSOCIATED_DOMAIN)
+  config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_ANDROID_GOOGLE_API
 
   if (!profile) {
     config.name = 'Vox Dev'
@@ -26,17 +28,13 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     config.android.package = `${basePackage}.staging`
     config.ios.googleServicesFile = './config/GoogleService-Info.plist'
     config.android.googleServicesFile = './config/google-services.json'
-    config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_ANDROID_GOOGLE_API_KEY_STAGING
     config.android.adaptiveIcon.foregroundImage = './assets/developement/adaptive-icon.jpg'
     config.ios.icon = './assets/developement/icon.jpg'
-    setAssociatedDomain(config, process.env.ASSOCIATED_DOMAIN_DEVELOPMENT)
   } else if (profile === 'production') {
     config.ios.bundleIdentifier = baseIdentifier
     config.android.package = basePackage
     config.ios.googleServicesFile = process.env.GOOGLE_SERVICES_IOS_PATH_PRODUCTION
     config.android.googleServicesFile = process.env.GOOGLE_SERVICES_ANDROID_PATH_PRODUCTION
-    config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_ANDROID_GOOGLE_API_KEY_PRODUCTION
-    setAssociatedDomain(config, process.env.ASSOCIATED_DOMAIN_PRODUCTION)
   } else if (profile === 'development') {
     config.name = 'Vox Dev'
     config.ios.bundleIdentifier = `${baseIdentifier}.${profile}`
@@ -45,8 +43,6 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     config.ios.icon = './assets/developement/icon.jpg'
     config.ios.googleServicesFile = process.env.GOOGLE_SERVICES_IOS_PATH_DEVELOPMENT
     config.android.googleServicesFile = process.env.GOOGLE_SERVICES_ANDROID_PATH_DEVELOPMENT
-    config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_ANDROID_GOOGLE_API_KEY_STAGING
-    setAssociatedDomain(config, process.env.ASSOCIATED_DOMAIN_DEVELOPMENT)
   } else {
     config.name = 'Vox Staging'
     config.android.adaptiveIcon.foregroundImage = './assets/staging/adaptive-icon.jpg'
@@ -55,8 +51,6 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     config.android.package = `${basePackage}.${profile}`
     config.ios.googleServicesFile = process.env.GOOGLE_SERVICES_IOS_PATH_STAGING
     config.android.googleServicesFile = process.env.GOOGLE_SERVICES_ANDROID_PATH_STAGING
-    config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_ANDROID_GOOGLE_API_KEY_STAGING
-    setAssociatedDomain(config, process.env.ASSOCIATED_DOMAIN_STAGING)
   }
 
   config.extra.storybookEnabled = process.env.STORYBOOK_ENABLED
