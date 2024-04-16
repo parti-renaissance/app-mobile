a84f46b204839940134f1d900ce695ab153a5e34
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-16 17:49:32 +0200

    #RE-1661 update modal (#704)
    * feature(RE-1661): check for update & modal
    
    * feature(RE-1661): check for update & modal
    
    * feature(RE-1661): don't do action if platform is web
    
    * feature(RE-1661): use app state instead of focus

---
+++ src/hooks/useAppUpdate.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react'
+import { checkVersion } from 'react-native-check-version'
+import { checkForUpdateAsync, fetchUpdateAsync, useUpdates } from 'expo-updates'
+import { isWeb } from 'tamagui'
+
+export default function useAppUpdate() {
+  const updates = useUpdates()
+  const [isBuildUpdateAvailable, setIsBuildUpdateAvailable] = useState(false)
+
+  const checkForUpdate = () => {
+    if (isWeb) {
+      return
+    }
+
+    const checkStoreUpdate = async () => {
+      const version = await checkVersion({
+        country: 'fr',
+      })
+
+      if (version.needsUpdate) {
+        setIsBuildUpdateAvailable(true)
+      }
+    }
+
+    const checkExpoUpdate = async () => {
+      try {
+        const update = await checkForUpdateAsync()
+
+        if (update.isAvailable) {
+          await fetchUpdateAsync()
+        }
+      } catch (error) {}
+    }
+
+    return Promise.allSettled([checkExpoUpdate(), checkStoreUpdate()])
+  }
+
+  useEffect(() => {
+    checkForUpdate()
+  }, [])
+
+  return {
+    isBuildUpdateAvailable,
+    checkForUpdate,
+    ...updates,
+  }
+}
