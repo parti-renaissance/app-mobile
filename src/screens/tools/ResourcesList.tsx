6fb66386b0efa63fd40626809c39db06840c2fa4
brandonsueur <gynidark@gmail.com>
2024-04-16 17:52:13 +0200

    feat(tools): new page Tools (#699)
    * feat(tools): new page Tools
    
    * refactor(tools): check if exist image in cardTool
    
    * refactor(tools): responsive cards tools with the media
    
    * fix: two columns with ScrollView
    
    * refactor: remvoe file sentryclirc
    
    * refactor(tools): ResourcesList, CardTool

---
+++ src/screens/tools/ResourcesList.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { RefreshControl, ScrollView } from 'react-native'
+import { useTools } from '@/hooks/useTools'
+import CardTool from '@/screens/tools/components/CardTool'
+import { getToken, useMedia, View } from 'tamagui'
+
+const ResourcesList = () => {
+  const media = useMedia()
+  const { data, refetch, isRefetching } = useTools()
+
+  const tools = data?.pages
+    .map((_) => _.items)
+    .flat()
+    ?.map((resource) => ({
+      type: 'GUIDE',
+      name: resource.label,
+      url: resource.url,
+      imageUrl: resource.image_url,
+    }))
+
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: getToken('$4', 'space'),
+        paddingTop: getToken(media.gtSm ? '$7' : '$4', 'space'),
+        paddingHorizontal: getToken(media.gtSm ? '$7' : '$4', 'space'),
+        paddingBottom: getToken('$6', 'space'),
+      }}
+      refreshControl={<RefreshControl refreshing={isRefetching} onRefresh={() => refetch()} />}
+    >
+      {tools?.map((item) => (
+        <View key={item.url} width={media.gtSm ? 'calc(50% - 16px)' : '100%'}>
+          <CardTool {...item} />
+        </View>
+      ))}
+    </ScrollView>
+  )
+}
+
+export default ResourcesList
