39ca9e177eca7ae945ccca47bf46f483127eeb79
Antonin <antonin.carlin@icloud.com>
2024-04-16 17:45:59 +0200

    feat: register

--- src/config/discoveryDocument.ts
+++ src/config/discoveryDocument.ts
@@ -1,13 +1,9 @@
-import type { DiscoveryDocument } from 'expo-auth-session';
-
+import type { DiscoveryDocument } from 'expo-auth-session'
 
 const discoveryDocument = {
   authorizationEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/oauth/v2/auth`,
   tokenEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/oauth/v2/token`,
-} as DiscoveryDocument
-
-    console.log(discoveryDocument)
-    console.log(process.env.EXPO_PUBLIC_API_BASE_URL)
-
+  registrationEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/inscription`,
+} satisfies DiscoveryDocument
 
 export default discoveryDocument
