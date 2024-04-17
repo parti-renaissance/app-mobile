e917d619936ccc6fde39fc32ca08c4506d5de7dc
Antonin <antonin.carlin@gmail.com>
2024-04-17 09:23:21 +0200

    fix: ressources #RE1781 (#707)

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,5 @@
-import { ConfigContext, ExpoConfig } from 'expo/config';
-import FontLib from './assets/fonts/generated-lib-fonts';
-
+import { ConfigContext, ExpoConfig } from 'expo/config'
+import FontLib from './assets/fonts/generated-lib-fonts'
 
 const baseIdentifier = 'fr.en-marche.jecoute'
 const basePackage = 'fr.en_marche.jecoute'
@@ -22,13 +21,18 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
   setAssociatedDomain(config, process.env.EXPO_PUBLIC_ASSOCIATED_DOMAIN)
   config.android.config.googleMaps.apiKey = process.env.EXPO_PUBLIC_ANDROID_GOOGLE_API
 
-  if (!profile) {
+  if (!profile || profile === 'development') {
     config.name = 'Vox Dev'
     config.scheme = 'vox-dev'
-    config.ios.bundleIdentifier = `${baseIdentifier}.developement`
-    config.android.package = `${basePackage}.staging`
-    config.ios.googleServicesFile = './config/GoogleService-Info.plist'
-    config.android.googleServicesFile = './config/google-services.json'
+    config.ios.bundleIdentifier = `${baseIdentifier}.development`
+    config.android.package = `${basePackage}.development`
+    if (profile === 'development') {
+      config.ios.googleServicesFile = process.env.GOOGLE_SERVICES_IOS_PATH_DEVELOPMENT
+      config.android.googleServicesFile = process.env.GOOGLE_SERVICES_ANDROID_PATH_DEVELOPMENT
+    } else {
+      config.ios.googleServicesFile = './config/GoogleService-Info.plist'
+      config.android.googleServicesFile = './config/google-services.json'
+    }
     config.android.adaptiveIcon.foregroundImage = './assets/developement/adaptive-icon.jpg'
     config.ios.icon = './assets/developement/icon.jpg'
   } else if (profile === 'production') {
@@ -36,16 +40,9 @@ export default ({ config }: ConfigContext): Partial<ExpoConfig> => {
     config.android.package = basePackage
     config.ios.googleServicesFile = process.env.GOOGLE_SERVICES_IOS_PATH_PRODUCTION
     config.android.googleServicesFile = process.env.GOOGLE_SERVICES_ANDROID_PATH_PRODUCTION
-  } else if (profile === 'development') {
-    config.name = 'Vox Dev'
-    config.ios.bundleIdentifier = `${baseIdentifier}.${profile}`
-    config.android.package = `${basePackage}.${profile}`
-    config.android.adaptiveIcon.foregroundImage = './assets/developement/adaptive-icon.jpg'
-    config.ios.icon = './assets/developement/icon.jpg'
-    config.ios.googleServicesFile = process.env.GOOGLE_SERVICES_IOS_PATH_DEVELOPMENT
-    config.android.googleServicesFile = process.env.GOOGLE_SERVICES_ANDROID_PATH_DEVELOPMENT
   } else {
     config.name = 'Vox Staging'
+    config.scheme = 'vox-staging'
     config.android.adaptiveIcon.foregroundImage = './assets/staging/adaptive-icon.jpg'
     config.ios.icon = './assets/staging/icon.jpg'
     config.ios.bundleIdentifier = `${baseIdentifier}.${profile}`
