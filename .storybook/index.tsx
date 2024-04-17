5c940f300b04c193179686ac6196a1cbb3026dd2
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-17 17:20:20 +0200

    feature(RE-1739): Add story book stories & set sidebar by default

--- .storybook/index.tsx
+++ .storybook/index.tsx
@@ -4,6 +4,7 @@ import { view } from './storybook.requires'
 const StorybookUIRoot = view.getStorybookUI({
   port: 7007,
   host: 'localhost',
+  tabOpen: -1,
   storage: {
     getItem: AsyncStorage.getItem,
     setItem: AsyncStorage.setItem,
