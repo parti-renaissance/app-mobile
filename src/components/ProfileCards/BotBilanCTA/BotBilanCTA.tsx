16361f5c690b01ac1858d7fbfa9c6f739f924f75
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-18 17:04:58 +0200

    feature(RE-1739): Add links to CTAs

--- src/components/ProfileCards/BotBilanCTA/BotBilanCTA.tsx
+++ src/components/ProfileCards/BotBilanCTA/BotBilanCTA.tsx
@@ -1,8 +1,14 @@
+import { useCallback } from 'react'
 import Button from '@/components/Button'
 import { ProfileCallToActionLayout } from '@/components/ProfileCards/ProfileCallToActionLayout/ProfileCallToActionLayout'
+import { openBrowserAsync } from 'expo-web-browser'
 import { gray } from '../../../../theme/colors.hex'
 
 export default function BotBilanCTA() {
+  const onPress = useCallback(() => {
+    openBrowserAsync('https://t.me/bilandeurope_bot')
+  }, [])
+
   return (
     <ProfileCallToActionLayout backgroundColor={'transparent'} noPadding>
       <ProfileCallToActionLayout.Content
@@ -15,7 +21,7 @@ export default function BotBilanCTA() {
         compact
       />
       <ProfileCallToActionLayout.Actions>
-        <Button variant={'outlined'} marginTop={'$2'} marginLeft={'$2.5'} zIndex={10}>
+        <Button variant={'outlined'} marginTop={'$2'} marginLeft={'$2.5'} zIndex={10} onPress={onPress}>
           <Button.Text>Ouvrir</Button.Text>
         </Button>
       </ProfileCallToActionLayout.Actions>
