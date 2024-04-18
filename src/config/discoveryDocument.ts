23c1ef36c6c44fb727e32e058d9a2d6cbfb98b04
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-18 17:15:38 +0200

    feature(RE-1739): Add links to CTAs (#716)

--- src/config/discoveryDocument.ts
+++ src/config/discoveryDocument.ts
@@ -3,7 +3,7 @@ import type { DiscoveryDocument } from 'expo-auth-session'
 const discoveryDocument = {
   authorizationEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/oauth/v2/auth`,
   tokenEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/oauth/v2/token`,
-  registrationEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/inscription`,
+  registrationEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/inscription?utm_source=app`,
 } satisfies DiscoveryDocument
 
 export default discoveryDocument
