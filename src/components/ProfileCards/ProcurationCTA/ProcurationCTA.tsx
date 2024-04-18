a8d203d2786e2e372eec9c1137cd9714ca0c4b22
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-18 17:15:38 +0200

    feature(RE-1739): Add links to CTAs (#716)

--- src/components/ProfileCards/ProcurationCTA/ProcurationCTA.tsx
+++ src/components/ProfileCards/ProcurationCTA/ProcurationCTA.tsx
@@ -1,8 +1,14 @@
+import { useCallback } from 'react'
 import Button from '@/components/Button'
 import { ProfileCallToActionLayout } from '@/components/ProfileCards/ProfileCallToActionLayout/ProfileCallToActionLayout'
+import { openBrowserAsync } from 'expo-web-browser'
 import { gray } from '../../../../theme/colors.hex'
 
 export default function ProcurationCTA() {
+  const onPress = useCallback(() => {
+    openBrowserAsync('https://besoindeurope.fr/procurations')
+  }, [])
+
   return (
     <ProfileCallToActionLayout backgroundColor={'#DEECF1'} noPadding>
       <ProfileCallToActionLayout.Content
@@ -15,7 +21,7 @@ export default function ProcurationCTA() {
         compact
       />
       <ProfileCallToActionLayout.Actions>
-        <Button variant={'outlined'} marginTop={'$2'} marginLeft={'$2.5'} zIndex={10}>
+        <Button variant={'outlined'} marginTop={'$2'} marginLeft={'$2.5'} zIndex={10} onPress={onPress}>
           <Button.Text>Accéder aux démarches</Button.Text>
         </Button>
       </ProfileCallToActionLayout.Actions>
