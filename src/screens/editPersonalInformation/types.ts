1b0882bcb9f59262eab5b5a61c8269a0c290f22c
Antonin <antonin.carlin@icloud.com>
2024-04-18 10:17:29 +0200

    refactor(profil): types & clean

--- src/screens/editPersonalInformation/types.ts
+++ src/screens/editPersonalInformation/types.ts
@@ -1,30 +1,10 @@
+import { z } from 'zod'
+import { PersonalInformationsFormSchema } from './validation'
+
 export enum Gender {
   Male = 'Male',
   Female = 'Female',
   Other = 'Other',
 }
 
-export interface Address {
-  address: string | undefined
-  postalCode: string | undefined
-  city: string | undefined
-  country: string | undefined
-}
-
-export interface PersonalInformationsForm {
-  firstName: string
-  lastName: string
-  gender: Gender
-  customGender: string | undefined
-  nationality: string
-  birthdate: Date
-  address: Address | undefined
-  email: string
-  phoneNumber: string
-  phoneCountryCode: string
-  facebook: string | undefined
-  twitter: string | undefined
-  instagram: string | undefined
-  linkedin: string | undefined
-  telegram: string | undefined
-}
+export type PersonalInformationsForm = z.infer<typeof PersonalInformationsFormSchema>
