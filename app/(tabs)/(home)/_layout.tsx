cf47e5901e4b08353655b5683b03fca080e3bedf
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-19 17:56:27 +0200

    fix(RE-1839): fix 404 on news after login (#726)

--- app/(tabs)/(home)/_layout.tsx
+++ app/(tabs)/(home)/_layout.tsx
@@ -9,6 +9,7 @@ export default function AppLayout() {
   }
   return (
     <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="(modals)/news-detail" options={{ headerShown: false }} />
       <Stack.Screen name="(modals)/event-detail" options={{ headerShown: false }} />
       <Stack.Screen name="(modals)/poll-detail" options={{ headerShown: false }} />
