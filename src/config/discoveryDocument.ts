654a16fd41a6be9f534bfc2968a4d92ac1961ef6
Antonin <antonin.carlin@icloud.com>
2024-04-22 19:41:47 +0200

    fix(register): use same login logic

--- src/config/discoveryDocument.ts
+++ src/config/discoveryDocument.ts
@@ -3,7 +3,7 @@ import type { DiscoveryDocument } from 'expo-auth-session'
 const discoveryDocument = {
   authorizationEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/oauth/v2/auth`,
   tokenEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/oauth/v2/token`,
-  registrationEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/inscription?utm_source=app`,
+  registrationEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/inscription`,
   endSessionEndpoint: `${process.env.EXPO_PUBLIC_OAUTH_BASE_URL}/deconnexion`,
 } satisfies DiscoveryDocument
 
--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -2,6 +2,8 @@ import discoveryDocument from '@/config/discoveryDocument'
 import * as AuthSession from 'expo-auth-session'
 import { maybeCompleteAuthSession } from 'expo-web-browser'
 import useBrowserWarmUp from './useBrowserWarmUp'
+import * as WebBrowser from 'expo-web-browser'
+import { th } from 'date-fns/locale'
 
 maybeCompleteAuthSession()
 
@@ -16,13 +18,19 @@ if (!process.env.EXPO_PUBLIC_ASSOCIATED_DOMAIN) {
 export const REDIRECT_URI = AuthSession.makeRedirectUri()
 const BASE_REQUEST_CONFIG = { clientId: process.env.EXPO_PUBLIC_OAUTH_CLIENT_ID, redirectUri: REDIRECT_URI }
 
-export const useCodeAuthRequest = () => {
+export const useCodeAuthRequest = (props?: {register: boolean}) => {
   return AuthSession.useAuthRequest(
     {
       ...BASE_REQUEST_CONFIG,
       scopes: ['jemarche_app', 'read:profile', 'write:profile'],
+      usePKCE: false,
+      extraParams: {
+        utm_source: 'app',
+      },
     },
-    discoveryDocument,
+    props && props?.register ? {
+      authorizationEndpoint: discoveryDocument.registrationEndpoint,
+    } : discoveryDocument,
   )
 }
 
@@ -32,7 +40,6 @@ const exchangeCodeAsync = ({ code, code_verifier }: { code: string; code_verifie
     {
       ...BASE_REQUEST_CONFIG,
       code,
-      extraParams: code_verifier ? { code_verifier } : undefined,
     },
     discoveryDocument,
   )
@@ -41,10 +48,13 @@ const exchangeCodeAsync = ({ code, code_verifier }: { code: string; code_verifie
 export const useLogin = () => {
   useBrowserWarmUp()
   const [response, , promptAsync] = useCodeAuthRequest()
-  return (code?: string) =>
-    code
-      ? exchangeCodeAsync({ code })
-      : promptAsync({
+  return (code?: string) => {
+    if (code) {
+      WebBrowser.dismissAuthSession()
+     return exchangeCodeAsync({ code, code_verifier: response?.codeVerifier})
+
+    } else {
+      return promptAsync({
           createTask: false,
         }).then((codeResult) => {
           if (codeResult.type === 'success') {
@@ -53,6 +63,25 @@ export const useLogin = () => {
           }
           return null
         })
+      }
+    }
+}
+
+
+export const useRegister = () => {
+  useBrowserWarmUp()
+  const [response, , promptAsync] = useCodeAuthRequest({register: true})
+      return () => promptAsync({
+          createTask: false,
+        }).then((codeResult) => {
+          if (codeResult.type === 'success') {
+            const code = codeResult.params.code
+            return exchangeCodeAsync({ code, code_verifier: response?.codeVerifier})
+          } else {
+            throw new Error('Error during registration')
+          }
+        })
+   
 }
 
 export default useLogin
