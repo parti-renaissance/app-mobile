4f1d864cbdf8b9f74b6ae1f1ae1b69d58010d0a7
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-24 20:03:07 +0200

    RE-1735 Profile / Disconnect popover (#748)
    * feature(popover): RE-1711 Profile popover
    
    * fix(popover): RE-1735 remove dead code
    
    * fix(popover): RE-1735 Improve position & trigger
    
    * fix(popover): RE-1735 remove dead code

---
+++ src/components/ProfilePopover/ProfilePopover.tsx
@@ -0,0 +1,44 @@
+import { PropsWithChildren, useCallback, useRef } from 'react'
+import { useSession } from '@/ctx/SessionProvider'
+import { router } from 'expo-router'
+import { isWeb, ListItem, Popover, Separator, YGroup } from 'tamagui'
+
+export default function ProfilePopover({ children }: PropsWithChildren) {
+  const { signOut } = useSession()
+
+  const ref = useRef<Popover>(null)
+
+  const onGoToProfile = useCallback(() => {
+    ref.current?.close()
+    router.push('/profil/')
+  }, [])
+
+  const onDisconnect = useCallback(async () => {
+    ref.current?.close()
+    await signOut()
+  }, [])
+
+  return (
+    <Popover ref={ref} hoverable allowFlip placement={'bottom'} offset={{ mainAxis: isWeb ? 10 : 60, crossAxis: -20 }}>
+      <Popover.Trigger cursor={'pointer'}>{children}</Popover.Trigger>
+
+      <Popover.Content borderWidth={1} padding={0} borderColor="$gray4">
+        <YGroup width={300}>
+          <MenuEntry onPress={onGoToProfile} title="Mon profil" />
+          <Separator borderStyle={'dashed'} borderColor="$gray4" />
+          <MenuEntry onPress={onDisconnect} title="Me déconnecter" danger />
+        </YGroup>
+      </Popover.Content>
+    </Popover>
+  )
+}
+
+const MenuEntry = ({ title, onPress, danger }: { title: string; onPress: () => void; danger?: boolean }) => (
+  <YGroup.Item>
+    <ListItem hoverTheme onPress={onPress} cursor={'pointer'}>
+      <ListItem.Text color={danger ? '$textDanger' : undefined} cursor={'pointer'}>
+        {title}
+      </ListItem.Text>
+    </ListItem>
+  </YGroup.Item>
+)
