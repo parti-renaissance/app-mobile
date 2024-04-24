7106fd2527cc5f53cb04fe2745d12082709e653b
Nicolas Guillard <nicolas@nitrique.fr>
2024-04-24 16:57:29 +0200

    fix(profile form): RE-1870 Phone number should not be mandatory

--- src/screens/editPersonalInformation/personalInformations.tsx
+++ src/screens/editPersonalInformation/personalInformations.tsx
@@ -93,7 +93,7 @@ const FormEditInformations = forwardRef<FormikProps<PersonalInformationsForm>, F
       validationSchema={toFormikValidationSchema(PersonalInformationsFormSchema)}
       onSubmit={(values) => handleOnSubmit(values)}
     >
-      {(formik) => (
+      {() => (
         <View gap="$7">
           <Text fontSize="$3" fontWeight="$6">
             Mes informations
