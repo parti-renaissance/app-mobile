0d956f7237b3b94857e7c6ac18c36a94bdbbd389
Antonin <antonin.carlin@gmail.com>
2024-04-24 16:44:13 +0200

    Update src/config/clientEnv.ts
    Co-authored-by: Nicolas Guillard <nicolas@nitrique.fr>

--- src/config/clientEnv.ts
+++ src/config/clientEnv.ts
@@ -16,7 +16,7 @@ const envClientSchema = z.object({
   ENVIRONMENT: z.enum(['staging', 'production']),
 })
 
-const unParsedEnvClient = {
+const unParsedClientEnv = {
   API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
   SENTRY_DSN: process.env.EXPO_PUBLIC_SENTRY_DSN,
   IOS_GOOGLE_API_KEY: process.env.EXPO_PUBLIC_IOS_GOOGLE_API_KEY,
