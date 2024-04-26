1d668e825bf77cc11dff404969e9811acc34b9e3
Antonin <antonin.carlin@icloud.com>
2024-04-26 17:41:49 +0200

    fix: blank img

--- src/components/Cards/EventCard/PartialEventCard.tsx
+++ src/components/Cards/EventCard/PartialEventCard.tsx
@@ -18,13 +18,14 @@ export type PartialEventVoxCardProps = {
 } & VoxCardFrameProps
 
 const PartialEventCard = ({ payload, onSubscribe, onShow, ...props }: PartialEventVoxCardProps) => {
+  const image = payload.image ?? require('@/assets/images/eventRestrictedImagePlaceholder.png')
   return (
     <VoxCard {...props}>
       <VoxCard.Content minHeight={200} justifyContent="space-between">
         <VoxCard.Content p={0}>
           <VoxCard.Chip event>Evenement</VoxCard.Chip>
           <VoxCard.Title>{payload.title}</VoxCard.Title>
-          <VoxCard.Image image={payload.image ?? require('@/assets/images/eventRestrictedImagePlaceholder.png')} />
+          {image && <VoxCard.Image image={image} />}
         </VoxCard.Content>
         <VoxCard.Date {...payload.date} />
         <XStack justifyContent={'flex-end'}>
