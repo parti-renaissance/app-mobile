260e4545662b185e7d3df96cc43b6bb3ae9127be
Antonin <antonin.carlin@icloud.com>
2024-04-27 13:00:41 +0200

    feat(Component): InnerAlert

---
+++ src/components/InternAlert/InternAlert.stories.tsx
@@ -0,0 +1,35 @@
+import { satisfies } from 'semver'
+import InternAlert, { InternAlertProps } from './InternAlert'
+
+export default {
+  title: 'InternAlert',
+  component: InternAlert,
+}
+
+export const Info = {
+  args: {
+    type: 'info',
+    children: 'Alert info lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  } satisfies InternAlertProps,
+}
+
+export const Danger = {
+  args: {
+    type: 'danger',
+    children: 'Alert danger lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  } satisfies InternAlertProps,
+}
+
+export const Warning = {
+  args: {
+    type: 'warning',
+    children: 'Alert warning lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  } satisfies InternAlertProps,
+}
+
+export const Success = {
+  args: {
+    type: 'success',
+    children: 'Alert success lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  } satisfies InternAlertProps,
+}
