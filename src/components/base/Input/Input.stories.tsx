ef201067ceafd6b143ecfd6c7f8828f72bc57084
Antonin <antonin.carlin@icloud.com>
2024-04-29 16:19:28 +0200

    feat: Input

--- src/components/base/Input/Input.stories.tsx
+++ src/components/base/Input/Input.stories.tsx
@@ -1,3 +1,4 @@
+import { Lock } from '@tamagui/lucide-icons'
 import { Stack } from 'tamagui'
 import Input from './Input'
 
@@ -9,16 +10,23 @@ export default {
 export function Default() {
   return (
     <Stack gap="$4">
-      <Input />
-      <Input error="une erreur" />
+      <Input placeholder="Je suis une placeholder" label="je suis un label" info="Je suis une info" />
+      <Input placeholder="Je suis un input qui charge !" label="je suis un label" info="Je suis une info" loading />
+      <Input placeholder="Je suis une placeholder" label="je suis un label" iconLeft={<Lock />} />
+      <Input placeholder="Je suis une placeholder" label="je suis un label" iconRight={<Lock />} />
+      <Input small placeholder="Je suis une petit" label="je suis un label" iconRight={<Lock />} />
+      <Input label="email" error="une erreur" value="J'ai écris n'imp !" info="je suis une info mais il ya une erreur" />
     </Stack>
   )
 }
 export function Minimal() {
   return (
     <Stack gap="$4">
-      <Input minimal />
-      <Input minimal error="une erreur" />
+      <Input minimal placeholder="Je suis une placeholder" label="je suis un label" info="Je suis une info" />
+      <Input minimal placeholder="Je charge " label="je suis un label" info="Je suis une info" loading />
+      <Input minimal placeholder="Je suis une placeholder" label="je suis un label" iconLeft={<Lock />} />
+      <Input minimal placeholder="Je suis une placeholder" label="je suis un label" iconRight={<Lock />} />
+      <Input minimal label="email" error="une erreur" value="J'ai écris n'imp !" info="je suis une info mais il ya une erreur" />
     </Stack>
   )
 }
