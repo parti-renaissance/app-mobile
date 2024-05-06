e2500252bf99dbd266c4ca78fc8baf088d819d4e
Nicolas Guillard <nicolas@nitrique.fr>
2024-05-06 10:24:30 +0200

    feature(header): RE-1737 bottom sheet disconnect (#771)

--- src/components/ProfilePopover/ProfilePopover.tsx
+++ src/components/ProfilePopover/ProfilePopover.tsx
@@ -1,10 +1,12 @@
 import { PropsWithChildren, useCallback, useRef } from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useSession } from '@/ctx/SessionProvider'
 import { router } from 'expo-router'
-import { isWeb, ListItem, Popover, Separator, YGroup } from 'tamagui'
+import { Adapt, isWeb, ListItem, Popover, Separator, YGroup } from 'tamagui'
 
 export default function ProfilePopover({ children }: PropsWithChildren) {
   const { signOut } = useSession()
+  const { bottom } = useSafeAreaInsets()
 
   const ref = useRef<Popover>(null)
 
@@ -22,8 +24,18 @@ export default function ProfilePopover({ children }: PropsWithChildren) {
     <Popover ref={ref} hoverable allowFlip placement={'bottom'} offset={{ mainAxis: isWeb ? 10 : 60, crossAxis: -20 }}>
       <Popover.Trigger cursor={'pointer'}>{children}</Popover.Trigger>
 
+      <Adapt when="sm" platform="touch">
+        <Popover.Sheet modal dismissOnSnapToBottom snapPointsMode="fit">
+          <Popover.Sheet.Handle />
+          <Popover.Sheet.Frame padding="$4" pb={bottom}>
+            <Adapt.Contents />
+          </Popover.Sheet.Frame>
+          <Popover.Sheet.Overlay animation="lazy" enterStyle={{ opacity: 0 }} exitStyle={{ opacity: 0 }} />
+        </Popover.Sheet>
+      </Adapt>
+
       <Popover.Content borderWidth={1} padding={0} borderColor="$gray4">
-        <YGroup width={300}>
+        <YGroup width={isWeb ? 300 : undefined}>
           <MenuEntry onPress={onGoToProfile} title="Mon profil" />
           <Separator borderStyle={'dashed'} borderColor="$gray4" />
           <MenuEntry onPress={onDisconnect} title="Me déconnecter" danger />
