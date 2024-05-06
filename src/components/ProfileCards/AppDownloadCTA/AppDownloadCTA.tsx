859e0d945242d6444c2660b9988a747cc8ce23e7
Nicolas Guillard <nicolas@nitrique.fr>
2024-05-06 18:40:29 +0200

    RE-1687 action page (#772)
    * feature(actions): RE-1687 Mobile wall wip
    
    * feature(actions): RE-1687 Resize buttons & responsive update

--- src/components/ProfileCards/AppDownloadCTA/AppDownloadCTA.tsx
+++ src/components/ProfileCards/AppDownloadCTA/AppDownloadCTA.tsx
@@ -1,25 +1,49 @@
 import { useCallback } from 'react'
 import { TouchableOpacity } from 'react-native'
+import Text from '@/components/base/Text'
 import { ProfileCallToActionLayout } from '@/components/ProfileCards/ProfileCallToActionLayout/ProfileCallToActionLayout'
 import redirectToStore from '@/helpers/redirectToStore'
-import { Image } from 'tamagui'
+import { Image, View } from 'tamagui'
 
-export default function AppDownloadCTA() {
+export interface AppDownloadCTAProps {
+  variant?: 'date' | 'screenshots'
+}
+
+export default function AppDownloadCTA({ variant = 'date' }: AppDownloadCTAProps) {
   const redirectToApple = useCallback(() => redirectToStore('ios'), [])
   const redirectToAndroid = useCallback(() => redirectToStore('android'), [])
 
+  const isDateVariant = variant === 'date'
+
   return (
     <ProfileCallToActionLayout>
-      <ProfileCallToActionLayout.Image source={require('./Assets/downloadCTA.png')} height={120} />
-      <ProfileCallToActionLayout.Content content={'Gardez la campagne à portée de main avec l’application mobile.'} textAlign={'center'} />
-      <ProfileCallToActionLayout.Actions>
-        <TouchableOpacity style={{ flex: 1 }} onPress={redirectToApple}>
-          <Image source={require('./Assets/Apple.png')} resizeMode={'contain'} width={'100%'} height={45} />
-        </TouchableOpacity>
+      <ProfileCallToActionLayout.Image
+        source={isDateVariant ? require('./Assets/downloadCTA.png') : require('./Assets/screenshots.png')}
+        height={isDateVariant ? 120 : 220}
+      />
+      <ProfileCallToActionLayout.Content>
+        <Text fontWeight={'$7'} textAlign={'center'} mb={isDateVariant ? undefined : '$4'}>
+          {isDateVariant
+            ? 'Gardez la campagne à portée de main avec l’application mobile.'
+            : 'Les Questionnaires et le Porte à porte sont pour le moment réservées à notre application mobile.'}
+        </Text>
 
-        <TouchableOpacity style={{ flex: 1 }} onPress={redirectToAndroid}>
-          <Image source={require('./Assets/Google.png')} resizeMode={'contain'} width={'100%'} height={45} />
-        </TouchableOpacity>
+        {!isDateVariant && (
+          <Text fontWeight={'$4'} textAlign={'center'}>
+            Téléchargez-là pour profiter de toutes les fonctionnalités !
+          </Text>
+        )}
+      </ProfileCallToActionLayout.Content>
+      <ProfileCallToActionLayout.Actions>
+        <View flexDirection={'row'} flex={1} justifyContent={'center'} gap={'$3'}>
+          <TouchableOpacity onPress={redirectToApple}>
+            <Image source={require('./Assets/Apple.png')} resizeMode={'contain'} width={130} height={45} />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={redirectToAndroid}>
+            <Image source={require('./Assets/Google.png')} resizeMode={'contain'} width={130} height={45} />
+          </TouchableOpacity>
+        </View>
       </ProfileCallToActionLayout.Actions>
     </ProfileCallToActionLayout>
   )
