328c7b0ea7ddcdd5c99c5cecc22ae46eb79d54dc
Antonin <antonin.carlin@icloud.com>
2024-05-16 10:57:30 +0200

    feat: add eslint plugin react compiler

--- eslint.js
+++ eslint.js
@@ -1,9 +1,10 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  pluging: ['prettier'],
+  pluging: ['prettier', 'eslint-plugin-react-compiler'],
   extends: ['universe/native', 'prettier', 'plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended'],
   rules: {
     'no-console': 'warn',
+    'react-compiler/react-compiler': 2,
   },
 }
