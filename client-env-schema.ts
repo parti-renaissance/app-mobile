3a859e53887ed4266fa4fdf8b3e7a3ff85e1fa59
Nicolas Guillard <nicolas@nitrique.fr>
2024-05-17 11:00:21 +0200

    feature(autocomplete): RE-1995 use proxy for autocomplete

--- client-env-schema.ts
+++ client-env-schema.ts
@@ -1,7 +1,6 @@
 import { z } from 'zod'
 
 const envClientSchema = z.object({
-  ANDROID_GOOGLE_API_KEY: z.string(),
   API_BASE_URL: z.string().url(),
   APP_RENAISSANCE_HOST: z.string(),
   ASSOCIATED_DOMAIN: z.string(),
@@ -11,7 +10,6 @@ const envClientSchema = z.object({
   FB_MEASUREMENT_ID: z.string(),
   FB_PROJECT_ID: z.string(),
   FB_SENDER_ID: z.string(),
-  IOS_GOOGLE_API_KEY: z.string(),
   OAUTH_BASE_URL: z.string().url(),
   OAUTH_CLIENT_ID: z.string(),
   SENTRY_DSN: z.string().url(),
