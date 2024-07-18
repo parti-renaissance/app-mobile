631371f130caa366f2f40376b07f824020b9a87a
Dimitri Gritsajuk <gritsajuk.dimitri@gmail.com>
2024-07-18 14:31:25 +0200

    chore: remove ActionCard warnings

--- app/(tabs)/evenements/_layout.tsx
+++ app/(tabs)/evenements/_layout.tsx
@@ -9,7 +9,7 @@ export default function EventLayout() {
     <Stack screenOptions={{ header: (x) => <SmallHeader {...x} />, animation: 'slide_from_right' }}>
       <Stack.Screen
         name="index"
-        options={isAuth ? { title: 'événements' } : { header: (x) => <Header {...x} />, title: 'événements', headerLeft: () => <EuCampaignIllustration /> }}
+        options={isAuth ? { title: 'Événements' } : { header: (x) => <Header {...x} />, title: 'Événements', headerLeft: () => <EuCampaignIllustration /> }}
       />
       <Stack.Screen name="[mode]/filters" options={{ headerShown: false, presentation: 'modal' }} />
       <Stack.Screen name="[id]" options={{ title: '' }} />
