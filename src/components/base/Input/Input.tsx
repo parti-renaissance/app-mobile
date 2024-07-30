c28ca2d8cef1f1ac3ea23e4a952092b3c1bf49fd
Antonin <antonin.carlin@gmail.com>
2024-07-30 11:19:09 +0200

    feat: input v2 (#839)
    * feat: input v2
    
    * chore: grammar
    
    * fix register form types

--- src/components/base/Input/Input.tsx
+++ src/components/base/Input/Input.tsx
@@ -1,117 +1,234 @@
-import React, { ComponentProps, forwardRef, useEffect, useId } from 'react'
-import { type TextInput } from 'react-native'
-import { Input } from '@/components/Bento/Inputs/components/inputsParts'
-import { useForwardFocus } from '@/hooks/useForwardFocus'
+import { forwardRef, useEffect, useState } from 'react'
+import { NativeSyntheticEvent, TextInput, TextInputFocusEventData, TextInputProps } from 'react-native'
 import { useForwardRef } from '@/hooks/useForwardRef'
 import { AlertCircle } from '@tamagui/lucide-icons'
-import { isWeb, Spinner, Theme, XStack, YStack } from 'tamagui'
-
-/**
- * note: make sure to use the same width for the Input and the Input.Area
- */
+import { AnimatePresence, isWeb, Spinner, styled, Text, XStack, YStack } from 'tamagui'
+import { gray } from 'theme/colors.hsl'
 
 type InputProps = {
-  id?: string
-  small?: boolean
-  minimal?: boolean
+  size?: 'xs' | 'sm' | 'md' | 'lg'
+  color?: 'white' | 'gray'
   error?: string
   label?: string
-  info?: string
-  iconLeft?: React.ReactNode
-  iconRightPress?: () => void
-  iconRight?: React.ReactNode
+  placeholder?: string
+  disabled?: boolean
   loading?: boolean
+  iconLeft?: React.ReactNode
+  iconRight?: React.ReactNode
+  onChange?: (text: string) => void
+  onIconRightPress?: () => void
+  type?: 'text' | 'password' | 'email' | 'number'
   fake?: boolean
-  type?: string
-} & ComponentProps<typeof Input.Area>
+} & Omit<TextInputProps, 'placeholder' | 'onChange'>
 
-export default forwardRef<TextInput, InputProps>(function VoxInput(props, ref) {
-  const { size, minimal, error, label, info, iconLeft, iconRight, small, loading, ...rest } = props
-  const uniqueId = useId()
-  const id = `${uniqueId}-${props.id ?? ''}`
+const InputFrame = styled(XStack, {
+  name: 'Input',
+  gap: '$2.5',
+  width: '100%',
+  minWidth: 150,
+  minHeight: '$3.5',
+  alignItems: 'center',
+  borderRadius: '$10',
+  paddingHorizontal: '$5',
+  borderWidth: '$1',
+  borderColor: '$colorTransparent',
+  animation: 'bouncy',
+  hoverStyle: {
+    backgroundColor: 'rgba(237, 239, 242, 1)',
+    cursor: 'text',
+  },
+  focusStyle: {
+    borderColor: '$blue9',
+  },
+
+  disabledStyle: {
+    backgroundColor: '$gray2',
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+
+  variants: {
+    loading: {
+      true: {
+        paddingRight: '$2.5',
+      },
+    },
+    iconLeft: {
+      true: {
+        paddingLeft: '$2.5',
+      },
+    },
+    iconRight: {
+      true: {
+        paddingRight: '$2.5',
+      },
+    },
+    error: {
+      true: {
+        backgroundColor: '$red/8',
+        focusStyle: {
+          borderColor: '$red8',
+        },
+      },
+    },
+    color: {
+      white: {
+        backgroundColor: '$white1',
+      },
+      gray: {
+        backgroundColor: '$gray2',
+      },
+    },
+
+    size: {
+      xs: {
+        height: '$3.5',
+      },
+      sm: {
+        height: '$4',
+      },
+      md: {
+        height: '$4.5',
+      },
+      lg: {
+        height: 56,
+      },
+    },
+  },
+})
+
+export default forwardRef<TextInput, InputProps>(function Input(_props, ref) {
+  const {
+    size,
+    color,
+    error,
+    label,
+    placeholder,
+    disabled,
+    loading,
+    iconLeft,
+    iconRight,
+    type,
+    fake,
+    onFocus,
+    onBlur,
+    onChangeText,
+    onIconRightPress,
+    onChange,
+    ...inputProps
+  } = _props
+  const [isFocused, setIsFocused] = useState(false)
   const inputRef = useForwardRef(ref)
-  const focusTrigger = useForwardFocus(inputRef)
-  const textInfo = !error && info ? info : error
-  const theme = error ? 'red' : 'gray'
+  const isFailed = !!error
+  const handleFocus = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    setIsFocused(true)
+    onFocus?.(e)
+  }
+  const handleBlur = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    setIsFocused(false)
+    onBlur?.(e)
+  }
+
+  const handleValueChange = (text: string) => {
+    onChangeText?.(text)
+    onChange?.(text)
+  }
+
+  const handlePress = () => {
+    inputRef.current?.focus()
+  }
 
   useEffect(() => {
-    if (inputRef.current && props.type && isWeb) {
+    if (inputRef.current && type && isWeb) {
       // @ts-expect-error wrong type on input
-      inputRef.current.type = props.type
+      inputRef.current.type = type
     }
-  }, [props.type])
+  }, [type])
 
   return (
-    <Theme name={theme}>
-      <Input flex={1} size={size ?? (small ? '$2' : '$3')} theme="VoxInput">
-        <YStack width="100%" gap={minimal ? 'unset' : undefined}>
-          {label && (
-            <Input.Label htmlFor={id} size="$1" fontWeight="$4" paddingBottom={minimal ? 0 : undefined}>
-              {label}
-            </Input.Label>
-          )}
-          <Input.Box
-            minimal={minimal}
-            backgroundColor={minimal ? '$colorTransparent' : props.backgroundColor ?? '$backgroundColor'}
-            hoverStyle={{
-              backgroundColor: minimal ? '$colorTransparent' : undefined,
-            }}
-            pressStyle={{
-              backgroundColor: minimal ? '$colorTransparent' : undefined,
-            }}
-          >
-            {iconLeft && (
-              <Input.Icon paddingLeft={minimal ? 0 : undefined} {...focusTrigger}>
-                {iconLeft}
-              </Input.Icon>
-            )}
-            {props.fake ? (
-              <YStack height={35} onPress={props.onPress} flex={1}>
-                <XStack alignItems="center" justifyContent="center" flex={1}>
-                  <Input.Fake
-                    paddingHorizontal="$3"
-                    maxWidth={props.maxWidth}
-                    numberOfLines={1}
-                    paddingLeft={minimal || iconLeft ? 0 : '$2'}
-                    paddingRight={minimal || iconRight ? 0 : '$2'}
-                    borderBottomWidth={0}
-                  >
-                    {props.value && props.value.length > 0 ? props.value : props.placeholder}
-                  </Input.Fake>
-                </XStack>
-              </YStack>
-            ) : (
-              <Input.Area
-                ref={inputRef}
-                paddingLeft={minimal || iconLeft ? 0 : undefined}
-                paddingRight={minimal || iconRight ? 0 : undefined}
-                borderBottomWidth={0}
-                underlineColorAndroid={'transparent'}
-                {...rest}
-                onChangeText={props.onChangeText ?? ((x) => props.onChange?.({ target: { value: x } }))}
-                id={id}
-              />
-            )}
-            {(iconRight || loading) && (
-              <Input.Icon paddingRight={minimal ? 0 : undefined} onPress={props.iconRightPress ?? props.onPress ?? (() => inputRef.current.focus())}>
-                {loading ? <Spinner /> : iconRight}
-              </Input.Icon>
-            )}
-          </Input.Box>
-        </YStack>
-        {textInfo && (
-          <XStack alignItems="center">
-            {error && (
-              <Input.Icon paddingLeft={0} paddingRight="$2">
-                <AlertCircle size="$0.75" />
-              </Input.Icon>
-            )}
-            <Input.Info fontSize="$1" fontWeight="$4" color={'$textDisabled'}>
-              {textInfo}
-            </Input.Info>
-          </XStack>
+    <YStack gap="$1" flex={1}>
+      <InputFrame
+        disabled={disabled}
+        color={color ?? 'white'}
+        size={size ?? 'md'}
+        error={isFailed}
+        loading={loading}
+        forceStyle={isFocused ? 'focus' : undefined}
+        onPress={handlePress}
+        iconLeft={!!iconLeft}
+        iconRight={!!iconRight}
+      >
+        {!loading && iconLeft && (
+          <YStack height="100%" justifyContent="center">
+            {iconLeft}
+          </YStack>
         )}
-      </Input>
-    </Theme>
+        <YStack gap="$1" flex={1} height="100%" justifyContent="center" position="relative">
+          <AnimatePresence>
+            {label ||
+              (placeholder && inputProps.value && inputProps.value.length > 0 && (
+                <YStack
+                  gap="$1"
+                  flex={1}
+                  alignSelf="flex-start"
+                  justifyContent="center"
+                  position="absolute"
+                  top={0}
+                  left={0}
+                  animation="bouncy"
+                  enterStyle={{
+                    opacity: 0,
+                  }}
+                  exitStyle={{
+                    opacity: 0,
+                  }}
+                >
+                  <Text color="$textSecondary" fontSize={10}>
+                    {label ?? placeholder}
+                  </Text>
+                </YStack>
+              ))}
+          </AnimatePresence>
+          {fake ? (
+            <Text color={gray.gray8} fontSize={14} numberOfLines={1} borderBottomWidth={0}>
+              {inputProps.value}
+            </Text>
+          ) : (
+            <TextInput
+              style={{
+                color: gray.gray8,
+                padding: 0,
+                fontSize: 14,
+                width: '100%',
+                height: '100%',
+              }}
+              editable={!disabled}
+              ref={inputRef}
+              value={inputProps.value}
+              onChangeText={handleValueChange}
+              placeholderTextColor={gray.gray5}
+              placeholder={placeholder}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              {...inputProps}
+            />
+          )}
+        </YStack>
+        {!loading && iconRight && (
+          <YStack height="100%" justifyContent="center" onPress={onIconRightPress}>
+            {iconRight}
+          </YStack>
+        )}
+        {loading && <Spinner color="$blue7" />}
+      </InputFrame>
+      {error && (
+        <XStack gap="$1" alignItems="center">
+          <AlertCircle color="$red8" size={10} />
+          <Text color="$red8" fontSize={10}>
+            {error}
+          </Text>
+        </XStack>
+      )}
+    </YStack>
   )
 })
