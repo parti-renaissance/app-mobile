a056048f111666ba3f48f8e47883a0213c52e503
Antonin CARLIN <antonin.carlin@icloud.com>
2024-08-23 17:50:55 +0200

    fix(profil): elu block

--- src/screens/profil/donations/components/SubscribeDonationCard.tsx
+++ src/screens/profil/donations/components/SubscribeDonationCard.tsx
@@ -21,53 +21,45 @@ export default function (props: { subscription: RestDonationsResponse[number] })
   return (
     <VoxCard bg="$green1">
       <VoxCard.Content>
-        <XStack>
-          <BadgeFrame theme="green">
-            <XStack gap="$1.5" alignItems="center">
-              <Crown color="$color" size={10} />
-              <Text fontSize="$1" fontWeight="$6" color="$color">
-                Financeur
-              </Text>
+        <XStack gap="$4">
+          <YStack flex={1} gap="$3">
+            <XStack>
+              <BadgeFrame theme="green">
+                <XStack gap="$1.5" alignItems="center">
+                  <Crown color="$color" size={10} />
+                  <Text fontSize="$1" fontWeight="$6" color="$color">
+                    Financeur
+                  </Text>
+                </XStack>
+              </BadgeFrame>
             </XStack>
-          </BadgeFrame>
-        </XStack>
-        <XStack>
-          <YStack gap="$3" flex={1}>
-            <XStack alignItems="center">
-              <YStack gap="$2" flex={2}>
-                <Text fontSize="$2" fontWeight="$6">
-                  Vous êtes financeur depuis le {getHumanFormattedDate(props.subscription.date)}
-                </Text>
-                <Text fontSize="$2" color="$textSecondary">
-                  Un don est programmé mensuellement.
-                </Text>
-              </YStack>
-              <YStack flex={1} justifyContent="center" alignItems="flex-end">
-                <YStack
-                  flex={1}
-                  justifyContent="center"
-                  alignItems="center"
-                  borderWidth="$1"
-                  borderRadius="$5"
-                  borderColor="$green3"
-                  p="$3"
-                  height="$8"
-                  width="$8"
-                >
-                  <Text fontSize="$7" fontWeight="$8" color="$green7">
-                    {props.subscription.amount} €
+            <YStack gap="$3" flex={1}>
+              <XStack alignItems="center" flex={1}>
+                <YStack gap="$2" flex={2}>
+                  <Text fontSize="$2" fontWeight="$6">
+                    Vous êtes financeur depuis le {getHumanFormattedDate(props.subscription.date)}
+                  </Text>
+                  <Text fontSize="$2" color="$textSecondary">
+                    Un don est programmé mensuellement.
                   </Text>
                 </YStack>
-              </YStack>
-            </XStack>
-            <XStack gap="$3">
-              <Button theme="green" variant="soft" onPress={handlePress('monthly')} disabled={isPending}>
-                <Button.Text>Modifier</Button.Text>
-              </Button>
-              <Button theme="green" onPress={handlePress('dayly')} disabled={isPending}>
-                <Button.Text>Faire un don</Button.Text>
-              </Button>
-            </XStack>
+              </XStack>
+              <XStack gap="$3">
+                <Button theme="green" variant="soft" onPress={handlePress('monthly')} disabled={isPending}>
+                  <Button.Text>Modifier</Button.Text>
+                </Button>
+                <Button theme="green" onPress={handlePress('dayly')} disabled={isPending}>
+                  <Button.Text>Faire un don</Button.Text>
+                </Button>
+              </XStack>
+            </YStack>
+          </YStack>
+          <YStack justifyContent="center" alignItems="flex-end">
+            <YStack justifyContent="center" alignItems="center" borderWidth="$1" borderRadius="$5" borderColor="$green3" height="$9" width="$9">
+              <Text fontSize="$6" fontWeight="$8" color="$green7">
+                {props.subscription.amount} €
+              </Text>
+            </YStack>
           </YStack>
         </XStack>
       </VoxCard.Content>
