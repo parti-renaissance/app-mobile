be7c73e9e6943b0ed9c294e251e000f016dc8260
Antonin CARLIN <antonin.carlin@icloud.com>
2024-09-11 13:37:48 +0200

    chore: change register link

--- src/config/discoveryDocument.ts
+++ src/config/discoveryDocument.ts
@@ -4,7 +4,7 @@ import type { DiscoveryDocument } from 'expo-auth-session'
 const discoveryDocument = {
   authorizationEndpoint: `${clientEnv.OAUTH_BASE_URL}/oauth/v2/auth`,
   tokenEndpoint: `${clientEnv.OAUTH_BASE_URL}/oauth/v2/token`,
-  registrationEndpoint: `${clientEnv.OAUTH_BASE_URL}/inscription`,
+  registrationEndpoint: `${clientEnv.OAUTH_BASE_URL}/adhesion `,
   endSessionEndpoint: `${clientEnv.OAUTH_BASE_URL}/deconnexion`,
 } satisfies DiscoveryDocument
 
