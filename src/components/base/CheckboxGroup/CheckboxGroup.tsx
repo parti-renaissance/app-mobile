08bc250cc0d94dfbb31d79dac3b5b6ebb00c7a1d
Antonin CARLIN <antonin.carlin@icloud.com>
2024-09-19 11:57:28 +0200

    feat(components): checkboxes

---
+++ src/components/base/CheckboxGroup/CheckboxGroup.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import Checkbox from '@/components/base/Checkbox/Checkbox'
+import Text from '@/components/base/Text'
+import { styled } from '@tamagui/core'
+import { ThemeableStack, XStack } from 'tamagui'
+
+type CheckboxGroupProps = {
+  options: {
+    label: string
+    value: string
+  }[]
+  value: string[]
+  onChange: (value: string[]) => void
+}
+
+const CheckboxGroupFrame = styled(ThemeableStack, {
+  gap: '$3',
+})
+
+export default function CheckboxGroup({ options, value, onChange }: CheckboxGroupProps) {
+  const handlePress = (item: string) => () => {
+    if (value.includes(item)) {
+      onChange(value.filter((v) => v !== item))
+      return
+    }
+    onChange([...value, item])
+  }
+
+  const isChecked = (item: string) => value.includes(item)
+  return (
+    <CheckboxGroupFrame>
+      {options.map((option) => (
+        <XStack key={option.value} gap="$2" group onPress={handlePress(option.value)} alignItems="center" cursor="pointer">
+          <Checkbox checked={isChecked(option.value)} />
+          <Text.MD multiline>{option.label}</Text.MD>
+        </XStack>
+      ))}
+    </CheckboxGroupFrame>
+  )
+}
