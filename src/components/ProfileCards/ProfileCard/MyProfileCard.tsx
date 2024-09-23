57466dcf876513f3182c7c65a2c45b579cb37106
Dimitri <gritsajuk.dimitri@gmail.com>
2024-09-23 11:13:54 +0200

    Revert "[SwitchUser] use admin host to go to esCadre"

--- src/components/ProfileCards/ProfileCard/MyProfileCard.tsx
+++ src/components/ProfileCards/ProfileCard/MyProfileCard.tsx
@@ -2,12 +2,10 @@ import { useCallback } from 'react'
 import ProfileCard from '@/components/ProfileCards/ProfileCard/ProfileCard'
 import clientEnv from '@/config/clientEnv'
 import { useSession } from '@/ctx/SessionProvider'
-import { useUserStore } from '@/store/user-store'
 import { openURL } from 'expo-linking'
 
 export default function MyProfileCard() {
   const { user, session } = useSession()
-  const { user: credentials } = useUserStore()
   const profile = user?.data
 
   if (!profile) {
@@ -18,7 +16,7 @@ export default function MyProfileCard() {
     return null
   }
 
-  const onNavigateToCadre = useCallback(() => openURL(`${credentials?.isAdmin ? clientEnv.ADMIN_URL : clientEnv.OAUTH_BASE_URL}${profile.cadre_auth_path}`), [])
+  const onNavigateToCadre = useCallback(() => openURL(`${clientEnv.OAUTH_BASE_URL}${profile.cadre_auth_path}`), [])
 
   return (
     <ProfileCard
