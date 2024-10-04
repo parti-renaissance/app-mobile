b6e5a42c05220ea775c9ec43547a191a3f4cd5d4
Antonin CARLIN <antonin.carlin@icloud.com>
2024-10-04 15:26:32 +0200

    fix: profil nav desktop

--- app/(app)/profil/_layout.tsx
+++ app/(app)/profil/_layout.tsx
@@ -1,11 +1,73 @@
 import { NamedExoticComponent } from 'react'
-import { SmallHeader, VoxHeader } from '@/components/Header/Header'
+import { View } from 'react-native'
+import EuCampaignIllustration from '@/assets/illustrations/EuCampaignIllustration'
+import BoundarySuspenseWrapper from '@/components/BoundarySuspenseWrapper'
+import { NavBar, ProfileView, SmallHeader, VoxHeader } from '@/components/Header/Header'
+import PageLayout from '@/components/layouts/PageLayout/PageLayout'
+import SkeCard from '@/components/Skeleton/CardSkeleton'
 import { useSession } from '@/ctx/SessionProvider'
+import ProfilMenu from '@/screens/profil/menu/Menu'
+import { TabRouter } from '@react-navigation/native'
 import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 import type { IconProps } from '@tamagui/helpers-icon'
 import { ArrowLeft, CircleUser, HelpingHand, MessageCircle, Settings2, TreeDeciduous } from '@tamagui/lucide-icons'
-import { Redirect, router, Stack } from 'expo-router'
-import { useMedia } from 'tamagui'
+import { Link, Navigator, Redirect, router, Slot, Stack, useNavigation, usePathname } from 'expo-router'
+import { isWeb, useMedia, XStack, YStack } from 'tamagui'
+
+const Skeleton = () => (
+  <SkeCard>
+    <SkeCard.Content>
+      <SkeCard.Chip />
+      <SkeCard.Title />
+      <SkeCard.Image />
+      <SkeCard.Description />
+    </SkeCard.Content>
+  </SkeCard>
+)
+function CustomRouter() {
+  return (
+    <Navigator router={TabRouter}>
+      <VoxHeader justifyContent="space-between">
+        <Link href="/">
+          <EuCampaignIllustration cursor="pointer" />
+        </Link>
+        <NavBar />
+        <Link href="/profil">
+          <ProfileView />
+        </Link>
+      </VoxHeader>
+      <PageLayout>
+        <PageLayout.SideBarLeft>
+          <XStack justifyContent="flex-end">
+            <ProfilMenu />
+          </XStack>
+        </PageLayout.SideBarLeft>
+        <PageLayout.MainSingleColumn>
+          <BoundarySuspenseWrapper
+            fallback={
+              <YStack
+                gap={16}
+                $gtSm={{
+                  pt: '$8',
+                  pl: '$8',
+                  pr: '$8',
+                }}
+                pb={isWeb ? '$10' : '$12'}
+              >
+                {[1, 2, 3].map((x) => (
+                  <Skeleton key={x} />
+                ))}
+              </YStack>
+            }
+          >
+            <Slot />
+          </BoundarySuspenseWrapper>
+        </PageLayout.MainSingleColumn>
+        <PageLayout.SideBarRight />
+      </PageLayout>
+    </Navigator>
+  )
+}
 
 const createProfilHeader = (icon: NamedExoticComponent<IconProps>) => (props: NativeStackHeaderProps) => {
   const media = useMedia()
@@ -34,7 +96,7 @@ export default function AppLayout() {
     return <Redirect href={'/(app)/(tabs)/evenements/'} />
   }
 
-  return (
+  return media.md ? (
     <Stack screenOptions={{ header: (x) => <SmallHeader {...x} />, animation: media.gtSm ? 'none' : 'slide_from_right' }}>
       <Stack.Screen
         name="index"
@@ -76,5 +138,7 @@ export default function AppLayout() {
         }}
       />
     </Stack>
+  ) : (
+    <CustomRouter />
   )
 }
