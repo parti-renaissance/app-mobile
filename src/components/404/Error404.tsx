b8bdd6492c76c0f44939f6b3efa7fef11b0a2998
Antonin <antonin.carlin@gmail.com>
2024-10-30 11:06:21 +0100

    feat: new 404 page (#899)

--- src/components/404/Error404.tsx
+++ src/components/404/Error404.tsx
@@ -1,7 +1,8 @@
-import { Button } from '@/components'
+// import { Image } from 'react-native'
 import PageLayout from '@/components/layouts/PageLayout/PageLayout'
+// import { Image } from 'expo-image'
 import { Link } from 'expo-router'
-import { Image, View } from 'tamagui'
+import { Image, View, XStack, YStack } from 'tamagui'
 import Text from '../base/Text'
 import { VoxButton } from '../Button'
 
@@ -9,14 +10,36 @@ export default function Error404() {
   return (
     <PageLayout>
       <PageLayout.StateFrame justifyContent="center" $gtSm={{ mt: 0 }}>
-        <Image source={require('../../assets/images/blocs.png')} height={200} width={200} resizeMode={'contain'} />
-        <Text color="$gray6">Impossible de trouver la page demandée</Text>
+        <Image
+          source={require('../../assets/images/404.png')}
+          resizeMode="contain"
+          $md={{
+            width: 400,
+            height: 400,
+          }}
+          $sm={{
+            width: 300,
+            height: 300,
+          }}
+        />
+        <YStack gap={24} justifyContent="center" alignItems="center">
+          <YStack gap={16}>
+            <Text.LG semibold textAlign="center">
+              404
+            </Text.LG>
+            <Text.MD width={254} textAlign="center">
+              Il semblerait que cette porte ne mène à... rien. La page n’existe pas.
+            </Text.MD>
+          </YStack>
 
-        <View>
-          <Link href="/" asChild>
-            <VoxButton variant="text">Retour à l'accueil</VoxButton>
-          </Link>
-        </View>
+          <View>
+            <Link href="/" asChild>
+              <VoxButton variant="soft" size="lg">
+                Retour à l'accueil
+              </VoxButton>
+            </Link>
+          </View>
+        </YStack>
       </PageLayout.StateFrame>
     </PageLayout>
   )
